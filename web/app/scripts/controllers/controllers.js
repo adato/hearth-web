57eec8b279927bf4a21de16b4f8bfc0b06b0a4f6
juhyxx <juhy@rezatyrakety.cz>
2014-04-18 22:01:02 +0200

    #53 Removed useless files

--- web/app/scripts/controllers/controllers.js
+++
@@ -1,8 +0,0 @@
-var __indexOf = [].indexOf || function(item) {
-		for (var i = 0, l = this.length; i < l; i++) {
-			if (i in this && this[i] === item) return i;
-		}
-		return -1;
-	};
-
-angular.module('hearth.controllers', []);
