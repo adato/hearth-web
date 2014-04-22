04474bb3e915dd93f933576dea5d319514dc7f56
juhyxx <juhy@rezatyrakety.cz>
2014-04-22 15:31:22 +0200

    Issued #50 Avatar refactoring

--- app/app/scripts/directives/avatar.js
+++ app/app/scripts/directives/avatar.js
@@ -3,6 +3,14 @@
 angular.module('hearth.directives').directive('avatar', [
 
 	function() {
+		var sizes = {
+			small: 30,
+			normal: 50,
+			follow: 60,
+			'user-search': 80,
+			'large': 90
+		};
+
 		return {
 			restrict: 'E',
 			replace: true,
@@ -12,44 +20,29 @@ angular.module('hearth.directives').directive('avatar', [
 				'clickFn': '&click',
 				'type': '='
 			},
-			template: '<span ng-class="extraClass" ng-click="clickFn()" ><img ng-src="{{ image.src }}" height="{{ image.px }}" width="{{ image.px }}"></span>',
+			templateUrl: 'templates/avatar.html',
 			link: function(scope, el, attrs) {
 				scope.$watch('relation', function(val) {
-					return scope.extraClass = val;
+					scope.extraClass = val;
+					return scope.extraClass;
 				});
 				scope.$watch('type', function(val) {
-					if (val === 'Community') {
-						scope.defaultImageType = EMPTY_COMMUNITY_AVATAR_URL;
-					}
-					if (val === 'User' || val === null || val === undefined) {
-						return scope.defaultImageType = EMPTY_AVATAR_URL;
-					}
+					scope.defaultImageType = val === 'Community' ? EMPTY_COMMUNITY_AVATAR_URL : EMPTY_AVATAR_URL;
+
+					return scope.defaultImageType;
 				});
 				return scope.$watch('source', function(val) {
 					scope.image = {
 						name: 'normal',
 						src: scope.defaultImageType
 					};
-					if ((attrs["class"] != null) && attrs['class']) {
+					if (attrs['class']) {
 						scope.image.className = attrs['class'];
 					}
-					if (attrs.size === 'small') {
-						scope.image.px = 30;
-					}
-					if (attrs.size === 'normal') {
-						scope.image.px = 50;
-					}
-					if (attrs.size === 'follow') {
-						scope.image.px = 60;
-					}
-					if (attrs.size === 'user-search') {
-						scope.image.px = 80;
-					}
-					if (attrs.size === 'large') {
-						scope.image.px = 96;
-					}
-					if ((val != null) && (val[scope.image.name] != null) && val[scope.image.name]) {
-						return scope.image.src = val[scope.image.name];
+					scope.image.px = sizes[attrs.size];
+					if (val && val[scope.image.name]) {
+						scope.image.src = val[scope.image.name];
+						return scope.image.src;
 					}
 				});
 			}
