1b7ca71cde429a4109455f6acc19c861919aee91
juhyxx <juhy@rezatyrakety.cz>
2014-04-25 09:37:01 +0200

    Closes #53 Affected by bugfix #36

--- app/app/scripts/controllers/TermsCtrl.js
+++ app/app/scripts/controllers/TermsCtrl.js
@@ -3,6 +3,7 @@
 angular.module('hearth.controllers').controller('TermsCtrl', [
 	'$scope', 'LanguageSwitch',
 	function($scope, LanguageSwitch) {
+		$scope.showButton = true;
 		var updateTermsPath = function() {
 			$scope.termsPath = '../locales/' + LanguageSwitch.uses() + '/terms.html';
 			return $scope.termsPath;
