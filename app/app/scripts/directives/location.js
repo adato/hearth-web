cc0874cdbbb62d4f4f8fe8ffd6e9eaf5ae03c331
juhyxx <juhy@rezatyrakety.cz>
2014-04-28 09:10:09 +0200

    #6 Location - refactoring

--- app/app/scripts/directives/location.js
+++ app/app/scripts/directives/location.js
@@ -5,81 +5,44 @@ angular.module('hearth.directives').directive('location', function() {
 		restrict: 'E',
 		replace: true,
 		templateUrl: 'templates/location.html',
-		link: function(scope, el, attrs) {
-			var map,
-				marker,
-				markers = [],
-				searchField = $(el).children('input'),
-				mapOptions = {
+		link: function(scope, el) {
+			var marker,
+				searchBox = new google.maps.places.SearchBox($(el).children('input')[0]),
+				map = new google.maps.Map($(el).children('#map-canvas')[0], {
 					zoom: 6,
 					zoomControl: true,
 					mapTypeControl: false,
 					streetViewControl: false,
 					center: new google.maps.LatLng(0, 0)
+				}),
+				moveToCurrentLocation = function() {
+					if (navigator.geolocation) {
+						navigator.geolocation.getCurrentPosition(function(position) {
+							placeMarker(new google.maps.LatLng(position.coords.latitude, position.coords.longitude), map);
+						});
+					}
 				},
-				searchBox = new google.maps.places.SearchBox(searchField[0]);
-
-			map = new google.maps.Map($(el).children('#map-canvas')[0], mapOptions);
-
-			if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(function(position) {
-					var position = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-					map.setCenter(position);
+				placeMarker = function(position, map) {
+					if (marker) {
+						marker.setMap(null);
+					}
 					marker = new google.maps.Marker({
 						position: position,
 						map: map
 					});
-				});
-			}
+					map.panTo(position);
+				};
 
 			google.maps.event.addListener(map, 'click', function(e) {
 				placeMarker(e.latLng, map);
 			});
-
-			function placeMarker(position, map) {
-				if (marker) {
-					marker.setMap(null);
-				}
-				marker = new google.maps.Marker({
-					position: position,
-					map: map
-				});
-
-				map.panTo(position);
-			}
 			google.maps.event.addListener(searchBox, 'places_changed', function() {
 				var places = searchBox.getPlaces();
 
-				for (var i = 0, marker; marker = markers[i]; i++) {
-					marker.setMap(null);
-				}
-
-				markers = [];
-				var bounds = new google.maps.LatLngBounds();
-				for (var i = 0, place; place = places[i]; i++) {
-					var image = {
-						url: place.icon,
-						size: new google.maps.Size(71, 71),
-						origin: new google.maps.Point(0, 0),
-						anchor: new google.maps.Point(17, 34),
-						scaledSize: new google.maps.Size(25, 25)
-					};
-
-					var marker = new google.maps.Marker({
-						map: map,
-						icon: image,
-						title: place.name,
-						position: place.geometry.location
-					});
-
-					markers.push(marker);
-
-					bounds.extend(place.geometry.location);
-				}
-
-				map.fitBounds(bounds);
+				placeMarker(places[0].geometry.location, map);
 			});
+
+			moveToCurrentLocation();
 
 		}
 	};
