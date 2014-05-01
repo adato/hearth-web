5e103387c3d7914904a062294abca9b2341ed320
juhyxx <juhy@rezatyrakety.cz>
2014-05-01 16:25:02 +0200

    #3 Displaying date in adds list

--- app/app/scripts/controllers/CreatePostCtrl.js
+++ app/app/scripts/controllers/CreatePostCtrl.js
@@ -9,20 +9,22 @@ angular.module('hearth.controllers').controller('CreatePostCtrl', [
 		};
 		$scope.post = $scope.defaultPost;
 		$scope.$on('setDefaultPost', function($event, newitem) {
-			var save;
 			$scope.post = angular.copy(newitem || $scope.defaultPost);
-			save = function() {
+			$scope.post.save = function() {
 				$scope.post = this;
 				return $scope.createAd();
 			};
-			return $scope.post.save = save;
+			return $scope.post.save;
 		});
+
 		$scope.setDefaultPost = function(item) {
 			return $scope.$broadcast('setDefaultPost', item);
 		};
+
 		$scope.createAd = function() {
 			var query;
-			if (($scope.post != null) && ($scope.post.name != null) && ($scope.post.name.length != null)) {
+
+			if ($scope.post && $scope.post.name && $scope.post.name.length > 0) {
 				if ($scope.post.name.length < 3) {
 					return $scope.errors = new ResponseErrors({
 						status: 400,
@@ -38,11 +40,7 @@ angular.module('hearth.controllers').controller('CreatePostCtrl', [
 					});
 				} else {
 					$scope.sending = true;
-					if ($scope.post._id) {
-						query = $scope.updatePost($scope.post);
-					} else {
-						query = $scope.createPost($scope.post);
-					}
+					query = $scope[$scope.post._id ? 'updatePost' : 'createPost']($scope.post);
 					return query.then(function(createdItem) {
 						$scope.$emit('cancelCreatingAd');
 						$scope.$emit('cancelEditingAd');
@@ -58,21 +56,35 @@ angular.module('hearth.controllers').controller('CreatePostCtrl', [
 				}
 			}
 		};
+
 		$scope.autodetectPosition = function() {
 			return Geocoder.findMeAndGeocode().then(function(geocodedLocation) {
 				return $scope.post.location = Geocoder.latLonToGeoJson(geocodedLocation);
 			});
 		};
+
 		$scope.photoUploadSuccessful = function($event) {
 			var _ref, _ref1;
+
 			if ((((_ref = $event.target) != null ? _ref.response : void 0) != null) && ((_ref1 = $event.target.status) !== 404 && _ref1 !== 403 && _ref1 !== 500)) {
 				return $scope.post.attachments = [JSON.parse($event.target.response)];
 			}
 		};
+
 		$scope.createPost = function(post) {
-			var deferred, event;
+			var deferred, eventName;
+
 			deferred = $q.defer();
 			$scope.sent = false;
+
+			var date = post.date.match(/(^\d{2})\.(\d{2})\.(\d{4})$/);
+			if (date) {
+				post.date = new Date(parseInt(date[3], 10), parseInt(date[2], 10) - 1, parseInt(date[1], 10), 0, 0, 0).getTime();
+			} else {
+				date = post.date.match(/(^\d{2})\/(\d{2})\/(\d{4})$/);
+				post.date = new Date(parseInt(date[3], 10), parseInt(date[1], 10) - 1, parseInt(date[2], 10), 0, 0, 0).getTime();
+			}
+
 			PostsService.add(post).then(function(data) {
 				if (data) {
 					$scope.sent = true;
@@ -85,16 +97,18 @@ angular.module('hearth.controllers').controller('CreatePostCtrl', [
 				$scope.sending = false;
 				return deferred.reject(err);
 			});
-			event = post.type === 'need' ? 'post new wish' : 'post new offer';
-			$analytics.eventTrack(event, {
+			eventName = post.type === 'need' ? 'post new wish' : 'post new offer';
+			$analytics.eventTrack(eventName, {
 				category: 'Posting',
 				label: 'NP',
 				value: 7
 			});
 			return deferred.promise;
 		};
+
 		return $scope.updatePost = function(post) {
-			var deferred, event;
+			var deferred, eventName;
+
 			deferred = $q.defer();
 			$scope.sent = false;
 			PostsService.update(post).then(function(data) {
@@ -107,8 +121,8 @@ angular.module('hearth.controllers').controller('CreatePostCtrl', [
 				$scope.sending = false;
 				return deferred.reject(err);
 			});
-			event = post.type === 'need' ? 'update wish' : 'update offer';
-			$analytics.eventTrack(event, {
+			eventName = post.type === 'need' ? 'update wish' : 'update offer';
+			$analytics.eventTrack(eventName, {
 				category: 'Posting',
 				label: 'NP',
 				value: 7
