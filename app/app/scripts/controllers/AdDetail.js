b917443a17f57509e5abf8c32b01305f011cff07
juhyxx <juhy@rezatyrakety.cz>
2014-06-04 15:16:59 +0200

    #292 Added reply

--- app/app/scripts/controllers/AdDetail.js
+++ app/app/scripts/controllers/AdDetail.js
@@ -7,16 +7,58 @@
  */
 
 angular.module('hearth.controllers').controller('AdDetail', [
-	'$scope', 'AdDetailResource', '$routeParams',
+	'$scope', 'AdDetailResource', '$routeParams', 'PostsService',
 
-	function($scope, AdDetailResource, $routeParams) {
-		$scope.item = {};
+	function(scope, AdDetailResource, $routeParams, PostsService) {
+		scope.item = {};
+		scope.replyDisplayed = false;
+		scope.reply = {};
 
 		AdDetailResource.get({
 			id: $routeParams.id
 		}, function(data) {
-			$scope.item = data;
-			$scope.profile = data.author;
+			scope.item = data;
+			scope.profile = data.author;
 		});
+
+		scope.replyToAd = function() {
+			if (!scope.reply.message || scope.reply.message.length < 3) {
+				this.errors = new ResponseErrors({
+					status: 400,
+					data: {
+						name: 'ValidationError',
+						message: 'ERR_REPLY_EMPTY_MESSAGE'
+					}
+				});
+				return;
+			}
+			if ((scope.reply.agreed != null) && scope.reply.agreed === false) {
+				this.errors = new ResponseErrors({
+					status: 400,
+					data: {
+						name: 'ValidationError',
+						message: 'ERR_REPLY_PLEASE_AGREE'
+					}
+				});
+			}
+			if (!this.replyForm.$valid) {
+				return;
+			}
+			scope.replyToAdSubmitting = true;
+			return PostsService.reply(scope.reply).then(function() {
+				scope.replyToAdSubmitting = false;
+				scope.replyToAdSubmitted = true;
+				return $timeout(function() {
+					scope.ad = null;
+					$rootScope.$broadcast('cancelReplyingAd');
+					return delete this.errors;
+				}, 8000);
+			}).then(null, function() {
+				delete this.errors;
+				scope.replyToAdSubmitting = false;
+				return $scope.replyToAdSubmitting;
+			});
+		};
 	}
+
 ]);
