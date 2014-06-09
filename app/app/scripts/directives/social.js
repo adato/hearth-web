e6a8f8fa56d842d71235fb3fc1d4676739d974ad
juhyxx <juhy@rezatyrakety.cz>
2014-06-09 17:17:28 +0200

    #285 testing link of FB
    Source
    http://cookieshq.co.uk/posts/generate-a-share-with-facebook-link-that-embed-summary-title-images-but-without-og-data/

--- app/app/scripts/directives/social.js
+++ app/app/scripts/directives/social.js
@@ -18,10 +18,10 @@ angular.module('hearth.directives').directive('social', [
 			},
 			templateUrl: 'templates/social.html',
 			link: function(scope) {
-				scope.$watch('item', function(value) {
+				scope.$watch('item', function(item) {
 					var url = window.location.href.replace(window.location.hash, '');
-					if (value) {
-						url += '%23/ad/' + value;
+					if (item) {
+						url += '%23/ad/' + item._id;
 					}
 
 					angular.extend(scope, {
