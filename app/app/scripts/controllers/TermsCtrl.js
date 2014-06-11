0a0de5d13fa8fe69c3126dda51526b02e9e44999
juhyxx <juhy@rezatyrakety.cz>
2014-06-11 16:26:16 +0200

    Closes #341

--- app/app/scripts/controllers/TermsCtrl.js
+++ app/app/scripts/controllers/TermsCtrl.js
@@ -3,15 +3,15 @@
 /**
  * @ngdoc controller
  * @name hearth.controllers.TermsCtrl
- * @description 
+ * @description
  */
- 
+
 angular.module('hearth.controllers').controller('TermsCtrl', [
 	'$scope', 'LanguageSwitch',
 	function($scope, LanguageSwitch) {
 		$scope.showButton = true;
 		var updateTermsPath = function() {
-			$scope.termsPath = '../locales/' + LanguageSwitch.uses() + '/terms.html';
+			$scope.termsPath = '../locales/' + LanguageSwitch.uses().code + '/terms.html';
 			return $scope.termsPath;
 		};
 		$scope.$watch(function() {
