86398940aeaaf99a9849c26373ed6b77c3ad1158
juhyxx <juhy@rezatyrakety.cz>
2014-06-23 07:51:28 +0200

    #330 Refactoring

---
+++ app/app/scripts/directives/ad.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name hearth.directives.ad
+ * @description
+ * @restrict E
+ */
+angular.module('hearth.directives').directive('ad', [
+
+	function() {
+		return {
+			restrict: 'E',
+			replace: true,
+			transclude: true,
+			scope: {
+				item: '='
+			},
+			templateUrl: 'templates/ad.html',
+			link: function(scope) {
+				scope.edit = false;
+			}
+
+		};
+	}
+]);
