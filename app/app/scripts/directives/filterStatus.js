031b1c119abb7bfab1081f4c38eabaa37f5fe802
juhyxx <juhy@rezatyrakety.cz>
2014-07-15 14:29:56 +0200

    #330 Filter status translation

--- app/app/scripts/directives/filterStatus.js
+++ app/app/scripts/directives/filterStatus.js
@@ -7,20 +7,34 @@
  * @restrict E
  */
 angular.module('hearth.directives').directive('filterStatus', [
-	'$location',
-	function($location) {
+	'$location', '$translate',
+
+	function($location, $translate) {
 		return {
 			restrict: 'E',
 			replace: true,
 			templateUrl: 'templates/directives/filterStatus.html',
 			link: function(scope) {
 				scope.$on('$routeUpdate', function() {
-					var searchParams = $location.search();
+					var searchParams = $location.search(),
+						related = searchParams.related;
+
 					scope.filterData = !$.isEmptyObject(searchParams) ? searchParams : undefined;
+					if (!$.isEmptyObject(searchParams)) {
+						related = related ? related.split(',') : [];
+
+						if (searchParams.my_section === 'true') {
+							related.push('just-mine-connection');
+						}
+						if (related) {
+							scope.filterData.related = $.map(related, function(text) {
+								return $translate(text);
+							});
+							scope.filterData.related = scope.filterData.related.join(', ');
+						}
+					}
 				});
-
 			}
-
 		};
 	}
 ]);
