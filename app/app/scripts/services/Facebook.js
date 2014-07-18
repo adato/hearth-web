88d5addf5b5edeb12df02cb1398535632ca8267a
junajan <jeezaa@seznam.cz>
2014-07-18 11:05:50 +0200

    FB invite friends functionality

---
+++ app/app/scripts/services/Facebook.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name hearth.services.Facebook
+ * @description
+ */
+
+angular.module('hearth.services').service('Facebook', [
+	'$translate',
+	function($translate) {
+		var inited = false;
+
+		this.init = function() {
+
+			if (!inited) {
+				FB.init({
+					appId: $$config.fbAppId,
+					cookie: true,
+					status: true,
+					xfbml: true
+				});
+			}
+			inited = true;
+		};
+
+		this.inviteFriends = function() {
+			FB.ui({
+				method: 'apprequests',
+				message: $translate('HOMEPAGE_HEADING')
+			});
+		}
+
+		this.init();
+		return this;
+	}
+]);
