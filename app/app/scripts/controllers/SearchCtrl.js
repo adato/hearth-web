9ef885ba7a90be3c319796a1b6884e60b5073447
junajan <jeezaa@seznam.cz>
2014-08-06 13:53:06 +0200

    Issue #396

--- app/app/scripts/controllers/SearchCtrl.js
+++ app/app/scripts/controllers/SearchCtrl.js
@@ -462,12 +462,13 @@ angular.module('hearth.controllers').controller('SearchCtrl', [
 		$scope.$watch('location.search().q', function(newval) {
 			if ((newval != null) && newval) {
 				$scope.srch.query = newval;
-				return $rootScope.$broadcast('searchWithRefresh');
+				// alert("AA3");
+				console.log("DEAD END - zdvojovani vysledku v search tabu");
+				// return $rootScope.$broadcast('searchWithRefresh');
 			}
 		});
 
 		$scope.$on('search', function() {
-
 			if ($scope.timeout) {
 				$timeout.clear($scope.timeout);
 			}
