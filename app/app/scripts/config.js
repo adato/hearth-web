a3fdc981e80a064cfb6f81b125f3060e204611ca
deepj <deepjungle.maca@gmail.com>
2014-08-12 00:19:32 +0200

    #$%!

--- app/app/scripts/config.js
+++ app/app/scripts/config.js
@@ -3,8 +3,8 @@ var $$config;
 $$config = {
 	apiPath: '/api',
 	gApiKey: 'AIzaSyDArE48iXuR16XEp8zEUDo4g6E7GSqGJgc',
-	// fbAppId: '277542219089599', // production
-	fbAppId: '769756073037691', // staging
+	fbAppId: '277542219089599', // production
+	// fbAppId: '769756073037691', // staging
 	features: {
 		aboutPage: false,
 		searchImage: false,
