549f281c4f805368352b6068bc748130ea5ba1c5
junajan <jeezaa@seznam.cz>
2014-08-13 12:43:07 +0200

    issue #470

---
+++ app/app/scripts/directives/fancybox.js
@@ -0,0 +1,32 @@
+'use strict';
+/**
+ * @ngdoc directive
+ * @name hearth.directives.fancybox
+ * @description Add fancybox page preview
+ * @restrict A
+ */
+angular.module('hearth.directives').directive('fancybox', function($compile, $timeout) {
+    return {
+        link: function($scope, element, attrs) {
+            $(element).find(".fancy").fancybox({
+                padding: 0,
+                helpers: {
+                    overlay: {
+                        locked: false
+                    }
+                },
+                hideOnOverlayClick: false,
+                hideOnContentClick: false,
+                enableEscapeButton: false,
+                showNavArrows: false,
+                onComplete: function() {
+                    $timeout(function() {
+                        $compile($("#fancybox-content"))($scope);
+                        $scope.$apply();
+                        $.fancybox.resize();
+                    })
+                }
+            });
+        }
+    }
+});
