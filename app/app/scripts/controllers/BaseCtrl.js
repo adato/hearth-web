2377f1e36cc2cc5faa0936f82b2fd22fc89dfd1c
junajan <jeezaa@seznam.cz>
2014-08-13 11:13:18 +0200

    Issue #490

--- app/app/scripts/controllers/BaseCtrl.js
+++ app/app/scripts/controllers/BaseCtrl.js
@@ -7,60 +7,66 @@
  */
 
 angular.module('hearth.controllers').controller('BaseCtrl', [
-	'$scope', '$location', '$route', 'Auth',
+    '$scope', '$location', '$route', 'Auth',
 
-	function($scope, $location, $route, Auth) {
-		var timeout;
+    function($scope, $location, $route, Auth) {
+        var timeout;
 
-		$scope.topArrowText = {};
-		$scope.isScrolled = false;
+        $scope.topArrowText = {};
+        $scope.isScrolled = false;
 
-		$scope.showUI = function(ui) {
-			$scope.$broadcast('showUI', ui);
-		};
-		$scope.logout = function() {
-			Auth.logout(function() {
-				window.location = window.location.pathname;
-			});
-		};
-		$scope.search = function(text) {
-			if(!text) return false;
-			$location.path('/search');
-			$location.search('q=' + (text || ""));
-		};
-		$scope.top = function() {
-			$('html, body').animate({
-				scrollTop: 0
-			}, 1000);
-		};
+        $scope.showUI = function(ui) {
+            $scope.$broadcast('showUI', ui);
+        };
+        $scope.logout = function() {
+            Auth.logout(function() {
+                window.location = window.location.pathname;
+            });
+        };
+        $scope.search = function(text) {
+            if (!text) return false;
+            $location.path('/search');
+            $location.search('q=' + (text || ""));
+        };
+        $scope.top = function() {
+            $('html, body').animate({
+                scrollTop: 0
+            }, 1000);
+        };
 
-		$scope.$watch('user', function() {
-			var user = $scope.user.get_logged_in_user;
-			if (user && user.avatar.normal) {
-				$scope.avatarExtraStyle = {
-					'background-image': 'url(' + user.avatar.normal + ')'
-				};
-			} else {
-				$scope.avatarExtraStyle = {
-					'background-image': 'url(' + $$config.defaultUserImage + ')'
-				};
-			}
-		});
+        $scope.$watch('user', function() {
+            var user = $scope.user.get_logged_in_user;
+            if (user && user.avatar.normal) {
+                $scope.avatarExtraStyle = {
+                    'background-image': 'url(' + user.avatar.normal + ')'
+                };
+            } else {
+                $scope.avatarExtraStyle = {
+                    'background-image': 'url(' + $$config.defaultUserImage + ')'
+                };
+            }
+        });
 
-		$scope.$on('$includeContentLoaded', function() {
-			if (timeout) {
-				clearTimeout(timeout);
-			}
-			timeout = setTimeout(function() {
-				$(document).foundation();
-			}, 1000);
-		});
+        $scope.$on('$includeContentLoaded', function() {
+            if (timeout) {
+                clearTimeout(timeout);
+            }
+            timeout = setTimeout(function() {
+                $(document).foundation();
+            }, 1000);
+        });
 
-		angular.element(window).bind('scroll', function() {
-			if ($(window).scrollTop() > 0 !== $scope.isScrolled) {
-				$('html').toggleClass('scrolled');
-				$scope.isScrolled = !$scope.isScrolled;
-			}
-		});
-	}
+        $scope.$on('$routeChangeSuccess', function(next, current) {
+            
+            $("#all").removeClass();
+            $("#all").addClass(current.controller);
+        });
+
+        angular.element(window).bind('scroll', function() {
+            if ($(window).scrollTop() > 0 !== $scope.isScrolled) {
+                $('html').toggleClass('scrolled');
+                $scope.isScrolled = !$scope.isScrolled;
+            }
+        });
+    }
 ]);
