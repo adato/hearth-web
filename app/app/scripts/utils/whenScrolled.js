800721b282960b7228ca5be5fba2e8b08782f5dd
junajan <jeezaa@seznam.cz>
2014-08-14 16:29:20 +0200

    Issue #581 20min

--- app/app/scripts/utils/whenScrolled.js
+++ app/app/scripts/utils/whenScrolled.js
@@ -12,22 +12,30 @@ angular.module('hearth.utils').directive('whenScrolled', function() {
 		restrict: 'A',
 		link: function(scope, element, attr) {
 			var lastRun = null,
-				raw = element[0];
+				raw = element[0],
+				offset = 1000;
 
-			angular.element(window).bind('scroll', function($event) {
+			function process ($event) {
 				var rectObject = raw.getBoundingClientRect(),
-					bottom = parseInt(rectObject.bottom, 10);
+					bottom = parseInt(rectObject.bottom, 10),
+					lock = false;
 
-				if (bottom > 0 && bottom <= parseInt(window.innerHeight)) {
+				if (bottom > 0 && bottom - offset <= parseInt(window.innerHeight) ) {
 					$event.stopPropagation();
 					$event.preventDefault();
-					if (lastRun + 2000 < new Date().getTime()) {
+					if (lastRun + 2000 < new Date().getTime() && ! lock) {
+						lock = true;
 						scope.$apply(attr.whenScrolled);
 						lastRun = new Date().getTime();
 						return lastRun;
 					}
+				} else {
+					lock = false;
 				}
-			});
+			}
+
+			angular.element(window).bind('scroll', process);
+			angular.element(window).bind('resize', process);
 		}
 	};
 });
