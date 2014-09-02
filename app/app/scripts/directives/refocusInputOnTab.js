cfc13cb8cd68b9718ea51e24c0f647682505d3f7
junajan <jeezaa@seznam.cz>
2014-09-02 22:13:26 +0200

    Submit tag with tab key

---
+++ app/app/scripts/directives/refocusInputOnTab.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name hearth.directives.scrollTo
+ * @description
+ */
+
+angular.module('hearth.directives').directive('refocusInputOnTab', [
+	'$timeout',
+	function($timeout) {
+		return {
+			restrict: 'A',
+			link: function(scope, element, attrs) {
+				$("input", element).keypress(function(e) {
+					if (e.keyCode == 9) {
+
+						e.preventDefault();
+						e.stopPropagation();
+
+						$("#searchBox").focus();
+						$(this).focus();
+					}
+				});
+			}
+		};
+	}
+]);
