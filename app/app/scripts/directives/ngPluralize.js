c370a35f78c8ecb49892ee723a22d7cd13bc1d8e
junajan <jeezaa@seznam.cz>
2014-09-17 13:56:24 +0200

    Directives for pluralized translations

---
+++ app/app/scripts/directives/ngPluralize.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name hearth.directives.ngPluralize
+ * @description 
+ * @restrict E
+ */
+
+angular.module('hearth.directives').directive('ngPluralize', function() {
+  return {
+    restrict : 'EA',
+    priority : 1, 
+    transclude : 'element',
+    link : function(scope, element, attrs, ctrl, transclude) {
+      scope.$watch(attrs.when, function(when) {
+        if (when) {
+          transclude(function(clone) {
+            element.replaceWith(clone);
+            element = clone;
+          });
+        }
+      });
+  }
+  };
+});
--- app/app/scripts/directives/pluralizeTranslate.js
+++ app/app/scripts/directives/pluralizeTranslate.js
@@ -4,7 +4,7 @@
  * @ngdoc directive
  * @name hearth.directives.pluralizeTranslate
  * @description 
- * @restrict A
+ * @restrict E
  */
 
 angular.module('hearth.directives').directive('pluralizeTranslate', [
@@ -17,20 +17,8 @@ angular.module('hearth.directives').directive('pluralizeTranslate', [
 				count: "=",
 				key: "=",
 			},
-			// template: '{{ translateDict | json }}',
-			// template: '<ng-pluralize count="count" when="{{ translateDict }}">'+'</ng-pluralize>',
 			template: '<span ng-pluralize count="count" when="translateStrings"></span>',
 			link: function($scope, el, attrs) {
-				$scope.translateDict = {};
-				$scope.count= 1;
-				console.log($scope.count);
-				console.log($scope.key);
-
-				$scope.translateStrings = {'0': '0 Süßigkeiten ausgewählt',
-					'one': '1 Süßigkeiten zum Verkauf',
-					'other': '{} Süßigkeiten zum Verkauf.'
-				};
-
 				$scope.$watch("key", function(key) {
 					$scope.translateStrings = JSON.parse($translate(key));
 				});
@@ -38,21 +26,3 @@ angular.module('hearth.directives').directive('pluralizeTranslate', [
 		};
 	}
 ]);
-
-angular.module('hearth.directives').directive('ngPluralize', function() {
-  return {
-    restrict : 'EA',
-    priority : 1, 
-    transclude : 'element',
-    link : function(scope, element, attrs, ctrl, transclude) {
-      scope.$watch(attrs.when, function(when) {
-        if (when) {
-          transclude(function(clone) {
-            element.replaceWith(clone);
-            element = clone;
-          });
-        }
-      });
-  }
-  };
-});
