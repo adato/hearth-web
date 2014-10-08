3e483ec0fff7c520d5825e5194bd589ab578b021
junajan <jeezaa@seznam.cz>
2014-10-08 11:10:07 +0200

    Issue #631

---
+++ app/app/scripts/directives/communityItem.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name hearth.directives.communityItem
+ * @description
+ * @restrict E
+ */
+angular.module('hearth.directives').directive('communityItem', [
+    '$rootScope', 
+    function($$rootScope) {
+        return {
+            restrict: 'E',
+            replace: true,
+            transclude: true,
+            scope: {
+                item: '=',
+                user: '='
+            },
+            templateUrl: 'templates/directives/communityItem.html',
+            link: function(scope, element) {
+                
+            }
+        };
+    }
+]);
