3eea88626d79377c5cf75f5213ee45180b992e5f
junajan <jeezaa@seznam.cz>
2014-10-15 18:01:27 +0200

    Tutorial - center, small fixes in flexslider, added close icon on initial screen #665

--- app/app/scripts/directives/flexslider.js
+++ app/app/scripts/directives/flexslider.js
@@ -14,18 +14,13 @@ angular.module('hearth.directives').directive('flexslider', function() {
 
                 element.flexslider({
                     animation: "slide",
-                    itemWidth: 600,
-                    itemMargin: 50,
+                    itemWidth: 650,
+                    itemMargin: 0,
                     useCSS: false,
                     directionNav: false,
-                    // after: $scope.hideItem,
-                    slideshow: false
+                    slideshow: false,
                 });
             });
-
-            $scope.hideItem = function(slider) {
-                console.log(slider.currentSlide);
-            }
         }
     }
 });
