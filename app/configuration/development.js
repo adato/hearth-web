8cd0228e186032be941bc0a6614254a5efd376f5
junajan <jeezaa@seznam.cz>
2015-01-09 11:04:00 +0100

    Download locales from remote - #818
    Conflicts:
    	web/app/app/locales/cs/messages.json
    	web/app/app/locales/en/messages.json

--- app/configuration/development.js
+++ app/configuration/development.js
@@ -1,5 +1,11 @@
 var $$localConfig; // development
 
 $$localConfig = {
-	fbAppId: '1495788017321716' // dev
+	fbAppId: '1495788017321716', // dev
+	lang: {
+		"cs": "cs_CZ",
+		"de": "de_DE",
+		"en": "en_US",
+		"lang": false
+	}
 };
