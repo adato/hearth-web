eb18fb8c1157dceafb93aa6b239d7036ea800538
junajan <jeezaa@seznam.cz>
2015-01-29 14:14:12 +0100

    Test translations

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -748,13 +748,13 @@ module.exports = function(grunt) {
 		'usemin',
 		'htmlmin',
 		'htmlmin:distTemplates', // minify template files before concatenation
-		// 'html2js', 				// merge all templates to one js file
+		'html2js', 				// merge all templates to one js file
 		'concat:scripts',
 		'concat:config',
-		// 'replace:dist', 		// inject angular module for merged templates
+		'replace:dist', 		// inject angular module for merged templates
 		'uglify',
-		// 'replace:tmplMinify', 		// minify merged templates
-		// 'concat:tmpl',			// concat templates merged to JS into scripts
+		'replace:tmplMinify', 		// minify merged templates
+		'concat:tmpl',			// concat templates merged to JS into scripts
 		'cacheBust'
 	]);
 
