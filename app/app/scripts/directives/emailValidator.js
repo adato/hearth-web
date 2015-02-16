c60abda9f650fdbfc0ee3441794c2feb986613d1
Pavel Voska <pavel.voska@adato.cz>
2015-02-16 11:26:20 +0100

    Email validation during registration moved to directive (#1049) -- missing file

---
+++ app/app/scripts/directives/emailValidator.js
@@ -0,0 +1,30 @@
+'use strict';
+/**
+ * @ngdoc directive
+ * @name hearth.directives.emailValidator
+ * @description Get an email valiated against the server
+ * @restrict A
+ */
+angular.module('hearth.directives').directive('emailValidator',
+	['Email', '$q', 
+	function( Email, $q ) {
+		return {
+			require: 'ngModel',
+			link: function(scope, element, attrs, ctrl) {
+				ctrl.$asyncValidators.used = function (modelValue, viewValue) {
+					var value = modelValue || viewValue;
+					var def = $q.defer();
+
+					Email.exists({ email: value }, function (ok) {
+						// email exists, ie is already registered
+						def.reject();
+					}, function (err) {
+						// can be registered
+						def.resolve();
+					});
+					return def.promise;
+				}
+			}
+		};
+	}]
+);
