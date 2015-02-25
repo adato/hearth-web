286b75528f2a2c42566c7aaa5bfb8058be576724
junajan <jeezaa@seznam.cz>
2015-02-25 12:39:04 +0100

    Added activity avatar directive

---
+++ app/app/scripts/directives/activityAvatar.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name hearth.directives.avatar
+ * @description 
+ * @restrict E
+ */
+
+angular.module('hearth.directives').directive('activityAvatar', [
+	function() {
+		return {
+			restrict: 'E',
+			replace: true,
+			scope: {
+				'type': '=',
+			},
+			template: '<span><avatar class="left" size="small" src="src"></avatar></span>',
+			link: function($scope, el, attrs) {
+				$scope.src = null;
+
+				$scope.$watch('type', function(val) {
+					if(!!~$$config.activitiesIcons.indexOf(val))
+						$scope.src = "images/icons/"+val+".png";
+					else
+						$scope.src = null;
+				});
+			}
+		};
+	}
+]);
