0cd23ee9d12b367156a9e8adc5b8cb4f961efea1
junajan <jeezaa@seznam.cz>
2015-02-27 13:24:22 +0100

    Staging config

--- app/configuration/staging.js
+++ app/configuration/staging.js
@@ -4,7 +4,13 @@ $$localConfig = {
 	fbAppId: '769756073037691', // stage
 	languages: {
 		"cs": "cs_CZ",
+		"sk": "sk_SK",
 		"en": "en_US",
+		"clean": false
 	},
-	apiPath: '/api'
+	oauth: {
+		facebook: "769756073037691",
+		google: '17571722659-lqdilmtd8eqkv7ru8f0j4aiuiegcn32q.apps.googleusercontent.com'
+	},
+	apiPath: 'https://api.stage.hearth.net'
 };
