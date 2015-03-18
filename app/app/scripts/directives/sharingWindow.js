c79b5c90f08eaabc74b961c1a9b2f84854267dec
cenda <mail@janjuna.cz>
2015-03-18 11:55:44 +0100

    Add directive which will open sharing links in popup windows

---
+++ app/app/scripts/directives/sharingWindow.js
@@ -0,0 +1,23 @@
+'use strict';
+/**
+ * @ngdoc directive
+ * @name hearth.directives.sharingWindow
+ * @description Opens sharing link in smaller window
+ * @restrict A
+ */
+angular.module('hearth.directives').directive('sharingWindow',
+	[
+	function() {
+		return {
+			restrict: 'A',
+			scope: false,
+			link: function(scope, element, attrs) {
+                element.bind('click', function(e){
+                	window.open(attrs.href, "Share content", "status = 1, width=550, height=400, resizable = 0" );
+                    e.preventDefault();
+                    e.stopPropagation();
+                })
+			}
+		};
+	}]
+);
