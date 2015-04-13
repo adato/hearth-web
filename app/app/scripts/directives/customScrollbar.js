11c5147d0cf1ddd02255a13dfa867531a97e92d6
cenda <mail@janjuna.cz>
2015-04-13 14:03:05 +0200

    Add custom scrollbar

---
+++ app/app/scripts/directives/customScrollbar.js
@@ -0,0 +1,23 @@
+'use strict';
+/**
+ * @ngdoc directive
+ * @name hearth.directives.customScrollbar
+ * @description Applies custom scrollbar to element
+ * @restrict A
+ */
+angular.module('hearth.directives').directive('customScrollbar', [
+    '$rootScope', '$timeout',
+    function($rootScope, $timeout) {
+        return {
+            restrict: 'A',
+            replace: true,
+            scope: false,
+            template: false,
+            link: function(scope, element, attrs) {
+                $timeout(function() {
+                    $(element).scrollbar();
+                });
+            }
+        };
+    }
+]);
