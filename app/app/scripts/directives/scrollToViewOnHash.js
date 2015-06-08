08aa3795bc399010522da5eeff4cc25ddb489196
Pavel Voska <pavel.voska@adato.cz>
2015-06-08 15:06:56 +0200

    Added feature HEARTH-158 - click on "x lidi odpovedelo" goes to detail and scrolls to replies

---
+++ app/app/scripts/directives/scrollToViewOnHash.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name hearth.directives.scrollToViewOnHash
+ * @description if set on element, the element will be scrolled to view using jquery animation when particular hash is in url (url:/page#hash)
+ * @restrict A
+ */
+
+angular.module('hearth.directives').directive('scrollToViewOnHash', [
+	'$location', '$timeout',
+	function($location, $timeout) {
+		return {
+			link: function(scope, el, attrs) {
+				var offset = 140; // px offset from top of target
+				var animationLength = 500;
+				if ($location.hash() !== attrs.scrollToViewOnHash) // go-on only on matching elements
+					return;
+
+				$timeout(function () {
+					var target = $(el);
+					$('html,body').stop().animate({scrollTop: target.offset().top - offset}, animationLength);
+				});
+			}
+		};
+	}
+]);
