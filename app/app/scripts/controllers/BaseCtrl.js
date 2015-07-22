64ec66d3994de779f3deaed9747be3d44300e93c
cenda <mail@junajan.cz>
2015-07-22 17:48:05 +0200

    Fix initial title

--- app/app/scripts/controllers/BaseCtrl.js
+++ app/app/scripts/controllers/BaseCtrl.js
@@ -52,7 +52,7 @@ angular.module('hearth.controllers').controller('BaseCtrl', [
             var state = $state.$current;
 
             // set new page title
-            if(state.title !== false && $rootScope.language)
+            if(state.title && $rootScope.language)
                 PageTitle.setTranslate('TITLE.'+(state.title || state.name));
         };
 
