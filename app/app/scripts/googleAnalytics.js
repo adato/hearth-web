bf228e12cf539345d0b82b02e9d245521aaa2615
cenda <mail@junajan.cz>
2015-07-29 10:53:38 +0200

    Added GA account for topmonks

--- app/app/scripts/googleAnalytics.js
+++ app/app/scripts/googleAnalytics.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// =================== ADATO GA ACCOUNT =======================
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -8,3 +9,13 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-40070733-1', {name: 'adato'});
 ga('adato.require', 'displayfeatures');
 ga('adato.send', 'pageview');
+
+
+// =================== TM GA ACCOUNT =======================
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-65720359-1', {name: 'topmonks'});
+ga('require', 'displayfeatures');
+ga('send', 'pageview');
