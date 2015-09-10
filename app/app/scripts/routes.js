e649ea3bbd9a59f143c97d49e9f90df4d0776344
dotTwelve <machacjan@gmail.com>
2015-09-10 09:19:46 +0200

    market responsive from wireframes

--- app/app/scripts/routes.js
+++ app/app/scripts/routes.js
@@ -21,6 +21,11 @@ angular.module('hearth').config([
 				templateUrl: 'templates/market.html',
 				controller: 'MarketCtrl',
 			})
+      .state('market-responsive', {
+        url: '/responsive',
+        templateUrl: 'templates/_responsive/market.html',
+        controller: 'MarketCtrl',
+      })
 			.state('market-refresh', {
 				url: "/market",
 				controller: ['$location', function($location) {
@@ -205,7 +210,7 @@ angular.module('hearth').config([
 				templateUrl: 'templates/error404.html',
 				controller: 'Error404Ctrl'
 			});
-			
+
 		$urlRouterProvider.otherwise(function($injector, $location){
 		   var state = $injector.get('$state');
 		   state.go('error404');
