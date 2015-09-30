5e3f2e292a85606053353454d2d01fba33ea6c96
Pavel Voska <pavel.voska@adato.cz>
2015-09-30 09:18:38 +0200

    Components page

---
+++ app/app/scripts/controllers/ComponentsCtrl.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc controller
+ * @name hearth.controllers.ComponentsController
+ * @description  used by "kitchen sink" page with all custom hearth components
+ */
+
+angular.module('hearth.controllers').controller('ComponentsCtrl', [
+    '$scope','KeywordsService',
+    function($scope, KeywordsService) {
+        $scope.components = { 
+                                "author" : { 
+                                    _id: '521f5d48b8f421d7200049d2', 
+                                    _type: 'User', 
+                                    avatar_large: null, 
+                                    size: [400, 400] 
+                                },
+                                "checkbox": [],
+                                "radio": false,
+                                "expandGroup": false,
+                                "authorSelectorDisabled": false,
+                                "authorList" : [{
+                                    _id: "52000f9d584b06020000004b",
+                                    _type: "User",
+                                    name: "Ashtar Sheran"
+                                }, {
+                                    _id: "520023efds06020000004b",
+                                    _type: "Community",
+                                    name: "Ashtars Sherans Community"
+                                }]
+
+                            };
+        $scope.queryKeywords = function($query) {
+            return KeywordsService.queryKeywords($query);
+        };
+	}
+]);
