09a268cfc165b46638d04dea804e44aca804f52b
cenda <mail@junajan.cz>
2015-10-12 16:37:30 +0200

    Change attribute names, remove valid_until_unlimited and fetch post detail before updating
    Conflicts:
    	web/app/app/scripts/controllers/_responsive/ResponsiveMarketCtrl.js
    	web/app/app/scripts/controllers/modal/ItemEdit.js

--- app/app/scripts/directives/item.js
+++ app/app/scripts/directives/item.js
@@ -79,7 +79,7 @@ angular.module('hearth.directives').directive('item', [
                     // is this my post? if so, show controll buttons and etc
                     scope.mine = scope.item.owner_id === ((scope.user) ? scope.user._id : null);
 
-                    scope.isExpiringSoon = !scope.item.valid_until_unlimited && moment(scope.item.valid_until).subtract(7, 'days').isBefore(new Date())
+                    scope.isExpiringSoon = !scope.item.valid_until == 'unlimited' && moment(scope.item.valid_until).subtract(7, 'days').isBefore(new Date())
                                                 && moment(scope.item.valid_until).isAfter(new Date());
 
                     // if the post is show instantly (without any effect) recount his height now
