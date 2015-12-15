d9bf67359e2dca10d7d5348cae6e83f39ab79487
cenda <mail@junajan.cz>
2015-12-15 12:04:22 +0100

    Fix map - HEARTH-447

--- app/app/scripts/bootstrap.js
+++
@@ -1,7 +0,0 @@
-// scripts to be loaded sooner
-preferredLanguage = $.cookie('language') || $$config.defaultLanguage;
-
-var googleApiScriptUrl = '//maps.googleapis.com/maps/api/js?' +
-	($$config && $$config['map'] && $$config['map']['apiKey'] ? 'key=' + $$config['map']['apiKey'] + '&' : '') +
-	'libraries=places,geometry&language=';
-document.write('<script id="google-maps-script" src="' + googleApiScriptUrl + preferredLanguage + '"><\/script>');
