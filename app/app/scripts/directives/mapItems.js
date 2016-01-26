27c771ad98257ba66e78790852d0ca5ebadeb172
Pavel Voska <pavel.voska@adato.cz>
2016-01-26 16:59:27 +0100

    test4

--- app/app/scripts/directives/mapItems.js
+++ app/app/scripts/directives/mapItems.js
@@ -44,6 +44,7 @@ angular.module('hearth.directives').directive('mapitems', [
 						return false;
 
 					map = geo.createMap($(".map-container")[0], options);
+					$scope.debugMap = map;
 					window.SCOPE = $scope;
 					$(".map-container", baseElement).addClass("inited");
 				};
