8574e3e4e3c32f9724731512948c1796cbafc099
kamil.novotny <novotny@blackglobe.cz>
2016-02-11 00:00:00 +0100

    HEARTH-489 flags added instead of globe

--- app/app/scripts/controllers/LanguageChangeCtrl.js
+++ app/app/scripts/controllers/LanguageChangeCtrl.js
@@ -9,7 +9,6 @@
 angular.module('hearth.controllers').controller('LanguageChangeCtrl', ['LanguageSwitch', '$rootScope', function(LanguageSwitch, $rootScope) {
 	var ctrl = this;
 	ctrl.currentLanguage = $rootScope.language;
-	//ctrl.languages = LanguageSwitch.getLanguages();
 	ctrl.switchLang = function(lang) {
 		LanguageSwitch.swicthTo(lang, true);
 	};
