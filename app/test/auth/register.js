df4389c43390f04479374e885232d25cc85d85dc
Pavel Voska <pavel.voska@adato.cz>
2016-02-11 10:29:28 +0100

    fix register and login test

--- app/test/auth/register.js
+++ app/test/auth/register.js
@@ -13,7 +13,7 @@ describe('hearth registration', function() {
     expect(element.all(by.css('.login-form')).isDisplayed()).toBeTruthy();
   	
     // go to register page
-  	element(by.css('.login-form a[href*="register"]')).click();
+  	element.all(by.css('.login-form a[href*="register"]')).get(1).click();
 
     // test if register form is visible
     expect(element.all(by.css('.register-form')).isDisplayed()).toBeTruthy();
