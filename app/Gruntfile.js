ff3f6ede071d9237255f82dd0a20fa864f704ffb
kamil.novotny <novotny@blackglobe.cz>
2016-02-15 02:29:02 +0100

    HEARTH-596 almost finished

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -424,7 +424,7 @@ module.exports = function(grunt) {
 				httpFontsPath: '../fonts',
 				relativeAssets: false,
 				assetCacheBuster: false,
-				bundleExec: false,
+				bundleExec: true,
 				raw: 'Sass::Script::Number.precision = 10\n',
 				importPath: [
 					'app/vendor/font-awesome/scss/',
