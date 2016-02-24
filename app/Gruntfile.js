28402a62fbb76b45510c58ac0ec61e5180302978
kamil.novotny <novotny@blackglobe.cz>
2016-02-24 12:21:49 +0100

    fe/hearth-616 wip

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -442,7 +442,7 @@ module.exports = function(grunt) {
 				httpFontsPath: '../fonts',
 				relativeAssets: false,
 				assetCacheBuster: false,
-				bundleExec: true,
+				bundleExec: false,
 				raw: 'Sass::Script::Number.precision = 10\n',
 				importPath: [
 					'app/vendor/font-awesome/scss/',
