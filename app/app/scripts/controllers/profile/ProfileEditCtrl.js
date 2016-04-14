7f55edb97de8ec5a298657de08f3b28f0274c3cb
dotTwelve <machacjan@gmail.com>
2016-04-14 16:23:06 +0200

    code format

--- app/app/scripts/controllers/profile/ProfileEditCtrl.js
+++ app/app/scripts/controllers/profile/ProfileEditCtrl.js
@@ -48,7 +48,7 @@ angular.module('hearth.controllers').controller('ProfileEditCtrl', [
 			$scope.filteredLangs.push(newLang);
 		});
 
-		function sortTranslations(a,b) {
+		function sortTranslations(a, b) {
 			return a.translate.localeCompare(b.translate);
 		}
 
