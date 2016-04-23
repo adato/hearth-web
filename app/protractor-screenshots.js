b3190e223ad0f6cf5e7729ffb261022b4c1f1630
Premek Maly <premma@gmail.com>
2016-04-23 17:57:15 +0200

    finding solution to connection timeout error on Jenkins CI server (cherry picked from commit 0d0e7a1)

--- app/protractor-screenshots.js
+++ app/protractor-screenshots.js
@@ -1,4 +1,4 @@
-var testFolder = 'test/';
+var testFolder = './test/';
 var HtmlScreenshotReporter = require('/usr/local/lib/node_modules/protractor-jasmine2-screenshot-reporter');
 
 var reporter = new HtmlScreenshotReporter({
@@ -7,44 +7,42 @@ var reporter = new HtmlScreenshotReporter({
 });
 
 exports.config = {
-//	seleniumAddress: 'http://localhost:4444/wd/hub',
-//  directConnect: true,
-//  capabilities: {
-//		'browserName': 'chrome' // or 'firefox', 'safari'
-//	},
-  specs: [
-    testFolder + 'unauth/marketplace.js',
-    testFolder + 'auth/register.js',
-    testFolder + 'auth/login.js',
-    testFolder + 'auth/bookmarks.js',
-    testFolder + 'auth/deleteUser.js'
-  ],
-  suites: {
-    'unauth': [testFolder + 'unauth/marketplace.js'],
-    'auth': [
-      testFolder + 'auth/register.js',
-      testFolder + 'auth/login.js',
-      testFolder + 'auth/deleteUser.js'
-    ]
-  },
+  directConnect: true,
+	specs: [
+		testFolder + 'unauth/marketplace.js',
+		testFolder + 'auth/register.js',
+		testFolder + 'auth/login.js',
+		testFolder + 'auth/bookmarks.js',
+		testFolder + 'auth/deleteUser.js'
+	],
+	suites: {
+		'unauth': [testFolder + 'unauth/marketplace.js'],
+		'auth': [
+			testFolder + 'auth/register.js',
+			testFolder + 'auth/login.js',
+			testFolder + 'auth/deleteUser.js'
+		]
+	},
+
   // Setup the report before any tests start
   beforeLaunch: function() {
     return new Promise(function(resolve){
       reporter.beforeLaunch(resolve);
     });
   },
-  onPrepare: function() {
-    protractor.helpers = require('./test/setup.js');
 
+	onPrepare: function() {
+		protractor.helpers = require('./test/setup.js');
     jasmine.getEnv().addReporter(reporter);
-  },
+	},
+
   // Close the report after all tests finish
   afterLaunch: function(exitCode) {
     return new Promise(function(resolve){
       reporter.afterLaunch(resolve.bind(this, exitCode));
     });
   },
-  params: {
-    env: 'local'
-  }
+	params: {
+		env: 'jenkins'
+	}
 };
