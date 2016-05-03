1294d0df8c2e1cf2499ab6d118698821e17464d1
dotTwelve <machacjan@gmail.com>
2016-05-03 15:25:28 +0200

    [BUG] HEARTH-734 dynamic loaded height on massages after attachment is added

--- app/app/scripts/directives/dynamicHeight.js
+++ app/app/scripts/directives/dynamicHeight.js
@@ -5,29 +5,38 @@
  * @description Makes element autoresizing based on current window height
  * @restrict A
  */
-angular.module('hearth.directives').directive('dynamicHeight', ['$timeout',
-	function($timeout) {
+angular.module('hearth.directives').directive('dynamicHeight', ['$timeout', '$window', '$rootScope',
+	function($timeout, $window, $rootScope) {
 		return {
 			restrict: 'A',
 			scope: {
 				minHeight: '=?',
 				offset: '=?',
-				container: '@?',
+				container: '@?'
 			},
 			link: function(scope, element, attrs) {
 				scope.minHeight = scope.minHeight || 200;
 
-				function resizeElement() {
-					var height = $(scope.container || window).height() - parseInt(scope.offset);
+				scope.onResize = function() {
+					var height = $(scope.container || $window).height() - parseInt(scope.offset);
 					$(element).height((height < scope.minHeight) ? scope.minHeight : height);
 				}
 
-				var timeout = $timeout(resizeElement);
-				$(window).resize(resizeElement);
+				var timeout = $timeout(scope.onResize());
+
+				angular.element($window).bind('resize', function() {
+					scope.onResize();
+				});
+
+				$rootScope.$on("dynamicHeightRedraw", function() {
+					$timeout(function() {
+						angular.element($window).triggerHandler('resize');
+					});
+				});
 
 				scope.$on('$destroy', function() {
 					$timeout.cancel(timeout);
-					$(window).unbind('resize', resizeElement);
+					angular.element($window).unbind('resize', resizeElement);
 				});
 			}
 		};
