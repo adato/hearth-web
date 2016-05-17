5fe0cff3583a8f4a2cc2912995763b4d23ad3a90
Pavel Voska <pavel.voska@gmail.com>
2016-05-17 14:27:19 +0200

    Updated max-len counter, added validation for invalid textareas

--- app/app/scripts/directives/communityCreateEdit.js
+++ app/app/scripts/directives/communityCreateEdit.js
@@ -124,7 +124,10 @@ angular.module('hearth.directives').directive('communityCreateEdit', [
 						data.webs = webs;
 					}
 					if (data.webs !== undefined) prepareWebs(data);
-					if (data.interests !== undefined) data.interests = data.interests.split(",");
+
+					if (typeof data.interests !== 'object') {
+						if (data.interests !== undefined) data.interests = data.interests.split(',');
+					}
 					return data;
 				};
 
@@ -169,6 +172,14 @@ angular.module('hearth.directives').directive('communityCreateEdit', [
 
 					if ($scope.communityForm.description.$invalid) {
 						$scope.showError.description = err = true;
+					}
+
+					if ($scope.communityForm.terms.$invalid) {
+						$scope.showError.terms = err = true;
+					}
+
+					if ($scope.communityForm.interests.$invalid) {
+						$scope.showError.interests = err = true;
 					}
 
 					if (!$scope.validateSocialNetworks()) {
