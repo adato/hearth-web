450c9467178d715901a88bd1f8de6aa112912482
dotTwelve <machacjan@gmail.com>
2016-06-06 13:00:00 +0200

    HEARTH-754 /app/map fix rendering on stage

--- app/app/scripts/geo/map.js
+++ app/app/scripts/geo/map.js
@@ -62,8 +62,9 @@ angular.module('hearth.geo').directive('map', [
 						height: 40,
 					}];
 
-				if (typeof templateSource !== 'string')
+				if (typeof templateSource !== 'string') {
 					templateSource = templateSource[1];
+				}
 
 				template = $interpolate(templateSource);
 
@@ -75,7 +76,7 @@ angular.module('hearth.geo').directive('map', [
 							});
 
 							google.maps.event.trigger(map, "resize");
-							geo.focusCurrentLocation();
+							geo.focusCurrentLocation(map);
 
 							oms = new OverlappingMarkerSpiderfier(map, {
 								markersWontMove: true,
@@ -99,17 +100,15 @@ angular.module('hearth.geo').directive('map', [
 				};
 
 				scope.testPositionLimit = function(loc) {
-
-					var lat = parseFloat(loc[0]).toFixed(4),
-						lng = parseFloat(loc[1]).toFixed(4),
-						key = "" + lat + ":" + lng;
+					var lat = parseFloat(loc[0]).toFixed(4);
+					var lng = parseFloat(loc[1]).toFixed(4);
+					var key = "" + lat + ":" + lng;
 
 					markerLimitValues[key] = markerLimitValues[key] ? markerLimitValues[key] + 1 : 1;
 					return markerLimitValues[key] > markerLimit;
 				};
 
 				scope.placeMarker = function(location, ad) {
-
 					var marker = geo.placeMarker(geo.getLocationFromCoords(location), ad[I_TYPE] == 0 ? 'need' : 'offer', ad);
 					oms.addMarker(marker);
 					markers.push(marker);
@@ -129,7 +128,6 @@ angular.module('hearth.geo').directive('map', [
 						var itemId = $(this).attr('itemid');
 
 						scope.$apply(function() {
-
 							var path = $location.path('post/' + itemId);
 						});
 					});
@@ -147,7 +145,6 @@ angular.module('hearth.geo').directive('map', [
 					Post.get({
 						postId: marker.info[I_ID]
 					}, function(data) {
-
 						data.author.avatar.normal = data.author.avatar.normal || $$config.defaultUserAvatar;
 						map.panTo(marker.position);
 
@@ -161,7 +158,6 @@ angular.module('hearth.geo').directive('map', [
 				};
 
 				scope.isInDistance = function(maxDist, base, point) {
-
 					var dist = google.maps.geometry.spherical.computeDistanceBetween(
 						new google.maps.LatLng(base.lat, base.lng), geo.getLocationFromCoords(point)
 					);
@@ -186,13 +182,11 @@ angular.module('hearth.geo').directive('map', [
 						};
 					}
 
-					// console.log("Nacetl jsem: " + ads.length);
 					for (i = 0; i < ads.length; i++) {
 						ad = ads[i];
 
 						for (j = 0; j < ad[I_LOCATION].length; j++) {
 							if (ad[I_LOCATION][j]) {
-
 								if (
 									(distance && !scope.isInDistance(distance, distanceBase, ad[I_LOCATION][j])) ||
 									markerLimit && scope.testPositionLimit(ad[I_LOCATION][j])
@@ -204,8 +198,10 @@ angular.module('hearth.geo').directive('map', [
 						}
 					}
 
-					if (scope.center)
+					if (scope.center) {
 						scope.centerZoomToAll(markers);
+					}
+
 					markerCluster.addMarkers(markers);
 					markerCluster.repaint();
 				};
