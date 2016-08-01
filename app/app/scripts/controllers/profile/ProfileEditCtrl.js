9d790f2e51601a5e4b99d9dce8f1c1ff07776f33
Gaspari <gaspari@russiamail.com>
2016-08-01 10:20:22 +0200

    allimportant 1 space autoformat

--- app/app/scripts/controllers/profile/ProfileEditCtrl.js
+++ app/app/scripts/controllers/profile/ProfileEditCtrl.js
@@ -196,7 +196,7 @@ angular.module('hearth.controllers').controller('ProfileEditCtrl', [
 			if (!data.interests) {
 				data.interests = [];
 			} else {
-				for (var i = data.interests.length;i--;) {
+				for (var i = data.interests.length; i--;) {
 					data.interests[i] = data.interests[i].term;
 				}
 			}
