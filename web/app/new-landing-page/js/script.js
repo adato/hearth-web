20e39e27e007c6eb5392759e420dbae211cf45f4
Gaspari <gaspari@russiamail.com>
2016-08-29 13:12:50 +0200

    HEARTH-761 header js refactor

--- web/app/new-landing-page/js/script.js
+++ web/app/new-landing-page/js/script.js
@@ -6,6 +6,7 @@
 		return document['querySelector' + (q.slice(0,1) === '#' ? '' : 'All')](q);
 	}
 	function fe(q, fn) {
+		if (! (q && q.length)) return;
 		for (var i = 0, l = q.length; i < l; fn(q[i]), i++);
 	}
 	function collectionHas(a, b) {
@@ -58,77 +59,129 @@
 	//
 	//	MENU AND LANGUAGE PANEL
 	//
-	var langToggle = 'language-toggle',
-		langPanel = 'language-panel',
-		isActive = 'is-active';
-	var hamburger = $('#hamburger');
-	var ltoggle = $('.' + langToggle);
-	var menu = $('#main-navigation');
-	var windowShown;
-	var action = true;
-	if (!(hamburger && menu)) throw new Error('all elements are required - hamburger, language-panel, main-navigation');
-	fe(ltoggle, function(el) {
-		el.addEventListener('click', function(event) {
-			toggleMenu(event, 'lang');
+	var isActive = 'is-active';
+
+	// var langToggle = 'language-toggle',
+	// 	langPanel = 'language-panel';
+	// var hamburger = $('#hamburger');
+	// var ltoggle = $('.' + langToggle);
+	// var menu = $('#main-navigation');
+	// var windowShown;
+	// var action = true;
+	// if (!(hamburger && menu)) throw new Error('all elements are required - hamburger, language-panel, main-navigation');
+	// fe(ltoggle, function(el) {
+	// 	el.addEventListener('click', function(event) {
+	// 		toggleMenu(event, 'lang');
+	// 	});
+	// });
+	// hamburger.addEventListener('click', function(event) {
+	// 	toggleMenu(event, 'menu');
+	// });
+	//
+	// function langHandler(lp) {
+	// 	menu.classList.remove(isActive);
+	// 	hamburger.classList.remove(isActive);
+	// 	// fe($('.' + langPanel), function(el) {el.classList.remove(isActive);});
+	//
+	// 	if (!action) return action = true;
+	// 	if (lp) lp.classList.toggle(isActive);
+	// }
+	// function menuHandler() {
+	// 	fe($('.' + langPanel), function(el) {el.classList.remove(isActive);});
+	//
+	// 	if (!action) return action = true;
+	// 	hamburger.classList.toggle(isActive);
+	// 	menu.classList.toggle(isActive);
+	// }
+	//
+	// function toggleMenu(event, w) {
+	// 	var toggle = event ? findParentBySelector(event.target, '.' + langToggle) : false;
+	// 	var lp;
+	// 	if (toggle) {
+	// 		lp = toggle.parentNode.getElementsByClassName(langPanel);
+	// 		if (!(lp && lp.length)) throw new Error('".language-panel" element required as a sibling to language-toggle.');
+	// 		lp = lp[0];
+	// 	}
+	// 	if (!windowShown) {
+	// 		window.addEventListener('mousedown', toggleHandler);
+	// 	} else {
+	// 		window.removeEventListener('mousedown', toggleHandler);
+	// 	}
+	// 	if (w === 'menu') {
+	// 		menuHandler();
+	// 	} else if (w === 'lang') {
+	// 		langHandler((lp ? lp : false));
+	// 	} else {
+	// 		fe($('.' + langPanel), function(el) {el.classList.remove(isActive);});
+	// 		menu.classList.remove(isActive);
+	// 		hamburger.classList.remove(isActive);
+	// 	}
+	// 	windowShown = w;
+	// }
+	//
+	// function toggleHandler(event) {
+	// 	if (!(findParentBySelector(event.target, '.hover-window'))) {
+	// 		var burgerAction = findParentBySelector(event.target, '#hamburger');
+	// 		var ltoggleAction = findParentBySelector(event.target, '#language-toggle');
+	// 		if ((burgerAction && menu.classList.contains('is-active')) || (ltoggleAction && lp.classList.contains('is-active'))) {
+	// 			action = false;
+	// 		}
+	// 		toggleMenu({}, false);
+	// 		window.removeEventListener('click', toggleHandler);
+	// 	} else if (event.target.tagName === 'A') {
+	// 		window.setTimeout(toggleMenu, 400);
+	// 	};
+	// }
+
+	//
+	//	NEW DROPDOWN HANDLER
+	//
+	fe($('[dropdown]'), function(el) {
+		el.addEventListener('click', function() {
+			var target = el.getAttribute('dropdown');
+
+			// hide all other dropdowns
+			fe($('[dropdown]:not([dropdown=' + target + '])'), function(otherEl) {
+				var otherELNode = $('#' + otherEl.getAttribute('dropdown'));
+				if (otherELNode) otherELNode.classList.remove(isActive);
+			});
+
+			// show target
+			var targetNode = $('#' + target);
+			if (targetNode) targetNode.classList.toggle(isActive);
+
+			// bind/unbind click on window
+			setTimeout(function(){
+				window[targetNode.classList.contains(isActive) ? 'addEventListener' : 'removeEventListener']('click', toggleHandler);
+			});
+
+			// dropdown options
+			var ddo = el.getAttribute('dropdown-opts');
+			if (ddo) {
+				try {
+					ddo = JSON.parse(ddo);
+					if (ddo.applyActiveToSelf) el.classList.toggle(isActive);
+				} catch (e) {console.info(e);}
+			}
 		});
 	});
-	hamburger.addEventListener('click', function(event) {
-		toggleMenu(event, 'menu');
-	});
-
-	function langHandler(lp) {
-		menu.classList.remove(isActive);
-		hamburger.classList.remove(isActive);
-		// fe($('.' + langPanel), function(el) {el.classList.remove(isActive);});
-
-		if (!action) return action = true;
-		if (lp) lp.classList.toggle(isActive);
-	}
-	function menuHandler() {
-		fe($('.' + langPanel), function(el) {el.classList.remove(isActive);});
-
-		if (!action) return action = true;
-		hamburger.classList.toggle(isActive);
-		menu.classList.toggle(isActive);
-	}
-
-	function toggleMenu(event, w) {
-		var toggle = event ? findParentBySelector(event.target, '.' + langToggle) : false;
-		var lp;
-		if (toggle) {
-			lp = toggle.parentNode.getElementsByClassName(langPanel);
-			if (!(lp && lp.length)) throw new Error('".language-panel" element required as a sibling to language-toggle.');
-			lp = lp[0];
-		}
-		if (!windowShown) {
-			window.addEventListener('mousedown', toggleHandler);
-		} else {
-			window.removeEventListener('mousedown', toggleHandler);
-		}
-		if (w === 'menu') {
-			menuHandler();
-		} else if (w === 'lang') {
-			langHandler((lp ? lp : false));
-		} else {
-			fe($('.' + langPanel), function(el) {el.classList.remove(isActive);});
-			menu.classList.remove(isActive);
-			hamburger.classList.remove(isActive);
-		}
-		windowShown = w;
-	}
-
 	function toggleHandler(event) {
 		if (!(findParentBySelector(event.target, '.hover-window'))) {
-			var burgerAction = findParentBySelector(event.target, '#hamburger');
-			var ltoggleAction = findParentBySelector(event.target, '#language-toggle');
-			if ((burgerAction && menu.classList.contains('is-active')) || (ltoggleAction && lp.classList.contains('is-active'))) {
-				action = false;
-			}
-			toggleMenu({}, false);
-			window.removeEventListener('click', toggleHandler);
-		} else if (event.target.tagName === 'A') {
-			window.setTimeout(toggleMenu, 400);
-		};
+			// close all dropdowns
+			fe($('[dropdown]'), function(el) {
+				var elNode = $('#' + el.getAttribute('dropdown'));
+				if (elNode) elNode.classList.remove(isActive);
+
+				// dropdown options
+				var ddo = el.getAttribute('dropdown-opts');
+				if (ddo) {
+					try {
+						ddo = JSON.parse(ddo);
+						if (ddo.applyActiveToSelf) el.classList.remove(isActive);
+					} catch (e) {console.info(e);}
+				}
+			});
+		}
 	}
 
 	//
@@ -194,22 +247,29 @@
 	//
 	var profile;
 
-	var loggedSelector = '.userLogged',
-		notLoggedSelector = '.userNotLogged',
-		profileSectionSelector = '#profileSection';
+	var apiPath = 'https://api.dev.hearth.net';
 
-	var apiToken = cookieFactory.get('authToken');
+	var loggedSelector = '[user-logged]',
+		notLoggedSelector = '[user-not-logged]',
+		profileSectionSelector = '#profileSection',
+		profileAvatarSelector = '[profile-image]',
+		profileFullNameSelector = '[profile-name]',
+		profileLinkSelector = '[profile-link]';
+
+	// var apiToken = cookieFactory.get('authToken');
+	var apiToken = 'siJsDpTNyUJ7x6xVyz9A';
 	if (apiToken) {
 		initProfile(apiToken);
 	} else {
-		fe($(notLoggedSelector), function(el) {el.style.display = ''});
+		fe($(notLoggedSelector), function(el) {el.style.display = 'inherit';el.classList.add('inited');});
+		fe($(loggedSelector), function(el) {el.style.display = 'none';el.classList.add('inited');});
 	}
 
 	///////////////////
 
 	function initProfile(apiToken) {
 		var xhr = new XMLHttpRequest();
-		xhr.open('GET', 'https://api.dev.hearth.net/profile');
+		xhr.open('GET', apiPath + '/profile');
 		xhr.setRequestHeader('X-API-TOKEN', apiToken);
 		xhr.setRequestHeader('Accept', 'application/vnd.hearth-v1+json');
 		xhr.setRequestHeader('X-API-VERSION', '1');
@@ -228,8 +288,12 @@
 	}
 
 	function fillProfile(profileObject) {
-		fe($(loggedSelector), function(el) {el.style.display = ''});
-		$(profileSectionSelector).innerHTML = (profile.avatar.normal ? '<img class="avatar" src="' + profile.avatar.normal + '" />' : '') + '<a href="/app/profile/' + profile._id + '" class="text-emphasize">' + [profile.name, profile.surname].join('\u00A0').trim() + '</a>';
+		fe($(loggedSelector), function(el) {el.style.display = '';el.classList.add('inited');});
+		fe($(notLoggedSelector), function(el) {el.style.display = 'none';el.classList.add('inited');});
+
+		fe($(profileAvatarSelector), function(el) {el.setAttribute('src', profile.avatar.normal);});
+		fe($(profileFullNameSelector), function(el) {el.innerHTML = [profile.name, profile.surname].join('\u00A0').trim();});
+		fe($(profileLinkSelector), function(el) {el.setAttribute('href', '/app/profile/' + profile._id);});
 	}
 
 })(window);
