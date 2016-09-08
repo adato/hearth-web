5e67f57f4fad5aeddd6668d37291add421f4938d
Gaspari <gaspari@russiamail.com>
2016-09-09 01:10:19 +0200

    HEARTH-761 changed lp folder structure, rewritten gruntfile, added minification and concat

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -1,49 +1,84 @@
 module.exports = function(grunt) {
 
+	var landingPageSrcFolder = 'landing-page-src',
+		landingPageDestFolder = 'app2';
+
+	var cssMinFiles = {};
+	cssMinFiles[landingPageDestFolder + '/css/main.css'] = [
+		landingPageSrcFolder + '/css/components.css',
+		landingPageSrcFolder + '/css/hamburger.css',
+		landingPageSrcFolder + '/css/dots.css',
+		landingPageSrcFolder + '/css/main.css',
+		landingPageSrcFolder + '/css/font-awesome.min.css'
+	];
+
 	grunt.initConfig({
 		clean: {
-
+			landingPageDestFolderCss: [landingPageDestFolder + '/css'],
+			landingPageDestFolderHtml: [landingPageDestFolder + '/**/*.html'],
+			landingPageDestFolderJs: [landingPageDestFolder + '/js'],
+			landingPageDestFolderFonts: [landingPageDestFolder + '/fonts'],
+			landingPageDestFolderImg: [landingPageDestFolder + '/img']
 		},
-		copy: {
-			
-		},
-		autoprefixer: {
+		concat: {
 			options: {
-				browsers: ['last 2 versions']
+				separator: ';',
 			},
 			dist: {
-				files: [{
-					expand: true,
-					cwd: '.tmp/css/',
-					src: '{,*/}*.css',
-					dest: '.tmp/css/'
-				}]
+    			src: [
+					landingPageSrcFolder + '/js/smooth-scroll.js',
+					landingPageSrcFolder + '/js/dots.js',
+					landingPageSrcFolder + '/js/script.js'
+				],
+    			dest: landingPageDestFolder + '/js/main.js',
+			},
+		},
+		copy: {
+			main: {
+				files: [
+					{expand: true, cwd: landingPageSrcFolder + '/fonts', src: ['**'], dest: landingPageDestFolder + '/fonts'},
+					{expand: true, cwd: landingPageSrcFolder + '/img', src: ['**'], dest: landingPageDestFolder + '/img'}
+				]
+			}
+		},
+		cssmin: {
+			options: {
+				sourcemap: false,
+	  			shorthandCompacting: false,
+				roundingPrecision: -1
+			},
+			target: {
+	  			files: cssMinFiles
 			}
 		},
 		i18n: {
     		dist: {
 	    		options: {
-			        baseDir: 'app/new-landing-page',
-			        outputDir: 'app/nlp',
+			        baseDir: landingPageSrcFolder + '/html',
+			        outputDir: landingPageDestFolder,
 	        	}
     		},
     		options: {
-		        // fileFormat: 'json',
-		        // exclude: [],
 		        locales: ['en', 'cs', 'sk'],
 		        locale: 'en',
 		        localesPath: 'locales',
 				selector: '[t]',
 				allowHtml: true,
-				removeAttr: true
+				//removeAttr: true
     		}
     	}
 	});
 
 	grunt.loadNpmTasks('grunt-i18n-static');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 
-	grunt.registerTask('default', ['clean', 'i18n', 'copy']);
+	// WARNING - watch out what is being cleaned !!!
+	grunt.registerTask('clean', ['clean']);
+
+	// default build task
+	grunt.registerTask('default', ['i18n', 'copy', 'cssmin', 'concat:dist']);
 
 };
