41405e7e74ef66c0c47ded0227b44e1ff66fed76
Pavel Voska <pavel.voska@gmail.com>
2016-09-12 15:27:41 +0200

    HEARTH-886 should only warn user if he has more than 80 percent of uppercase letters

--- app/app/scripts/directives/checkCapitals.js
+++ app/app/scripts/directives/checkCapitals.js
@@ -15,10 +15,25 @@ angular.module('hearth.directives').directive('checkCapitals', [
 			},
 			template: '<div><span ng-if="isLoud">{{:: "ERR_TEXT_TOO_LOUD" | translate }}</span></div>',
 			link: function(scope, element, attrs) {
+
 				scope.isLoud = false;
+				scope.threshold = 0.8; // 80% maximum uppercase letters
+
+				var isUppercase = function(value) {
+					var uppercased = 0;
+					value.split('').forEach(function(letter) {
+						// skip numbers
+						if (letter && parseInt(letter) != letter && letter.toUpperCase() === letter) {
+							uppercased++;
+						}
+					})
+					var uppercasePercent = uppercased / value.length;
+					return (uppercasePercent > scope.threshold);
+				}
+
 				scope.$watch('model', function(newVal, oldVal) {
-					if (typeof newVal != 'undefined' && newVal !== null && newVal.toUpperCase() === newVal) {
-						scope.isLoud = true;
+					if (typeof newVal != 'undefined' && newVal !== null && newVal != '') {
+						scope.isLoud = isUppercase(newVal);
 					}
 				});
 			}
