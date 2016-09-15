4f1d19f231cc03c1a260f2e1775b69c62be83a31
Gaspari <gaspari@russiamail.com>
2016-09-15 12:03:36 +0200

    HEARTH-761 cookie factory extended with path, lang cookie set to same path

--- web/landing-page-src/js/language.js
+++ web/landing-page-src/js/language.js
@@ -36,47 +36,49 @@
 
 	// language based on browser settings
 	var langBrowser = window.navigator.userLanguage || window.navigator.language;
-
-	// language based on url path
-	var langUrl = document.location.pathname.replace(/^\/|\/$/g, '').split('/').slice(0, 1).toString();
-
-	function changeLanguage(lang, relocate) {
-		console.log('Setting language to cookie:', lang);
-
-		window.aeg.cookieFactory.set(languageCookie, lang);
-		if (!relocate) return true;
-		setTimeout(function() {
-			return window.location = langPathMap[lang];
-		}, 100);
-	}
-
 	langBrowser = (langBrowser) ? langBrowser.substring(0, 2).toLowerCase() : defaultLanguage;
 	langBrowser = (langTranslationMap[langBrowser]) ? langTranslationMap[langBrowser] : defaultLanguage;
 
+	// language based on url path
+	var langUrl = document.location.pathname.replace(/^\/|\/$/g, '').split('/').slice(0, 1).toString();
 	if (langsAvailable.indexOf(langUrl) == -1) langUrl = defaultLanguage;
 
 	// ASSIGN LANG ATTRIBUTE TO HTML TAG
 	document.querySelector('html').setAttribute('lang', langUrl);
 
-	console.log('Languages are: browser =', langBrowser, '| url =', langUrl, '| cookie =', window.aeg.cookieFactory.get(languageCookie));
+	init();
 
-	if (/prerender|bot|googlebot|crawler|spider|robot|crawling/i.test(navigator.userAgent)) {
-	    // console.log('Don\'t change language for crawlers');
-	} else {
-	    console.log('Regular visitor - checking language');
+	/////////////////
 
-		// if there is not set language in cookie yet
-		// use language from browser
-		if (!window.aeg.cookieFactory.get(languageCookie)) {
-			console.log('There is no language in cookie yet');
-			changeLanguage(langBrowser);
+	function init() {
+
+		// console.log('Languages are: browser =', langBrowser, '| url =', langUrl, '| cookie =', window.aeg.cookieFactory.get(languageCookie));
+
+		if (/prerender|bot|googlebot|crawler|spider|robot|crawling/i.test(navigator.userAgent)) {
+		    // console.log('Don\'t change language for crawlers');
+		} else {
+		    // console.log('Regular visitor - checking language');
+
+			// if there is not set language in cookie yet
+			// use language from browser
+			if (!window.aeg.cookieFactory.get(languageCookie)) {
+				// console.log('There is no language in cookie yet');
+				changeLanguage(langBrowser);
+			}
+
+			// if we are not on right url, redirect
+			var cookieLang = window.aeg.cookieFactory.get(languageCookie);
+			if (langUrl !== cookieLang) {
+				// console.log('Language in URL is not the same as value in cookies, redirecting to ', cookieLang);
+				changeLanguage(cookieLang, true);
+			}
 		}
+	}
 
-		// if we are not on right url, redirect
-		if (langUrl !== window.aeg.cookieFactory.get(languageCookie)) {
-			console.log('Language in URL is not the same as value in cookies, redirecting to ', window.aeg.cookieFactory.get(languageCookie));
-			changeLanguage(defaultLanguage, true);
-		}
+	function changeLanguage(lang, relocate) {
+		window.aeg.cookieFactory.set(languageCookie, lang, '/');
+		if (!relocate) return true;
+		window.location = langPathMap[lang];
 	}
 
 })(window);
