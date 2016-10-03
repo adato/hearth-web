ba69f38e5b49a0040511fffa4496827b3328c814
Pavel Voska <pavel.voska@gmail.com>
2016-10-03 09:05:11 +0200

    debug in progress

--- app/app/scripts/geo/map.js
+++ app/app/scripts/geo/map.js
@@ -19,7 +19,8 @@ angular.module('hearth.geo').directive('map', [
 			replace: true,
 			scope: {
 				ads: "=",
-				center: "="
+				center: "=",
+				centerTo: "=",
 			},
 			// transclude: true,
 			link: function(scope, element) {
@@ -63,6 +64,11 @@ angular.module('hearth.geo').directive('map', [
 						height: 40,
 					}];
 
+				var idleListenerFunction = function() {
+					console.log('searching idle searchreq', map.getBounds().toJSON());
+					$rootScope.$emit('searchRequest', map.getBounds().toJSON());
+				}
+
 				if (typeof templateSource !== 'string') {
 					templateSource = templateSource[1];
 				}
@@ -70,6 +76,7 @@ angular.module('hearth.geo').directive('map', [
 				template = $interpolate(templateSource);
 
 				scope.initMap = function() {
+					console.log('init map');
 					if (!map) {
 						$timeout(function() {
 							map = geo.createMap(element[0], {
@@ -97,13 +104,12 @@ angular.module('hearth.geo').directive('map', [
 							//                            markerCluster.addListener('click', scope.zoomMarkerClusterer);
 							oms.addListener('click', scope.onMarkerClick);
 
+							google.maps.event.addListener(map, 'idle', idleListenerFunction);
+
 							/**
 							 *	Attach listener to idle state to refresh data.
 							 *	I am not using bounds_changed event, as it is buggy and fires multiple times per map drag.
 							 */
-							google.maps.event.addListener(map, 'idle', function() {
-								$rootScope.$emit('searchRequest', map.getBounds().toJSON());
-							});
 
 						}, 100);
 					}
@@ -143,11 +149,28 @@ angular.module('hearth.geo').directive('map', [
 					});
 				};
 
+				var extendBounds = function(markers) {
+					var bounds = new google.maps.LatLngBounds();
+					markers.forEach(function(marker) {
+						bounds.extend(marker.getPosition());
+					});
+
+					console.log('reduced bounds', bounds)
+					return bounds;
+				}
+
 				// this will zoom to show all markers and center map view
 				scope.centerZoomToAll = function(markers) {
-					map.fitBounds(markers.reduce(function(bounds, marker) {
-						return bounds.extend(marker.getPosition());
-					}, new google.maps.LatLngBounds()));
+					console.log('centerzoomall', markers);
+
+					google.maps.event.clearListeners(map, 'idle');
+
+					map.fitBounds(extendBounds(markers));
+
+					$timeout(function() {
+						console.log('re-set listener');
+						google.maps.event.addListener(map, 'idle', idleListenerFunction);
+					}, 100);
 				};
 
 				scope.onMarkerClick = function(marker) {
@@ -177,6 +200,7 @@ angular.module('hearth.geo').directive('map', [
 				}
 
 				scope.createPins = function(e, ads) {
+					console.log('creating pins', ads)
 					var i, j, ad, location, distanceBase, distance = false;
 					ads = ads || [];
 					markers = [];
@@ -213,6 +237,7 @@ angular.module('hearth.geo').directive('map', [
 							}
 						}
 					}
+					console.log('reinit occured, center =', scope.center)
 
 					if (scope.center) scope.centerZoomToAll(markers);
 					markerCluster.addMarkers(markers);
@@ -226,6 +251,30 @@ angular.module('hearth.geo').directive('map', [
 
 				scope.initMap();
 				scope.$on('showMarkersOnMap', scope.createPins);
+
+				scope.$watch('centerTo', function(newVal, oldVal) {
+					if (newVal !== oldVal && newVal !== null) {
+
+						google.maps.event.clearListeners(map, 'idle');
+
+						if (typeof newVal.lng === 'undefined') {
+							newVal.lng = newVal.lon;
+						}
+						map.setCenter(newVal);
+						map.setZoom(markerClusterMaxZoom + 1);
+						console.log('centerTo applied');
+
+
+
+						$rootScope.$emit('searchRequest', map.getBounds().toJSON());
+
+						$timeout(function() {
+							console.log('re-set listener2');
+							google.maps.event.addListener(map, 'idle', idleListenerFunction);
+						}, 100);
+
+					}
+				});
 			}
 		};
 	}
