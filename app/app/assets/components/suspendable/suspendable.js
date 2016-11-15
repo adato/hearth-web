e09e41fa6bc6084b8b398afc3e675795f0629efe
Pavel Voska <pavel.voska@gmail.com>
2016-11-15 10:10:53 +0100

    HEARTH-964 suspendable - comments

--- app/app/assets/components/suspendable/suspendable.js
+++ app/app/assets/components/suspendable/suspendable.js
@@ -1,8 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name hearth.directives.suspendable
+ * @description Makes item on a long list suspended when out-of-view. 
+ 								It moves $$watchers out of scope, and pushes them back on resume. 
+ 								It can save quite a lot of watchers in $digest cycle
+ * @restrict A
+ */
+
 angular.module('hearth.directives').directive('suspendable', function() {
 	return {
+		restrict: 'A',
 		link: function(scope, element, attr) {
 			var watchers, timeout, suspended = false;
 
+			// detects, if element is visible on screen (+- offset)
 			var isElementInView = function(element) {
 				var pageTop = $(window).scrollTop();
 				var pageBottom = pageTop + $(window).height();
@@ -13,12 +26,14 @@ angular.module('hearth.directives').directive('suspendable', function() {
 				return ((pageTop - offset < elementTop) && (pageBottom + offset > elementBottom));
 			}
 
+			// moves watchers out of scope
 			var suspend = function() {
 				watchers = scope.$$watchers;
 				scope.$$watchers = [];
 				suspended = true;
 			};
 
+			// resumes watchers
 			var resume = function() {
 				if (watchers)
 					scope.$$watchers = watchers;
@@ -32,6 +47,7 @@ angular.module('hearth.directives').directive('suspendable', function() {
 				clearTimeout(timeout);
 			})
 
+			// main function to loop over
 			var checkVisibility = function() {
 				if (isElementInView(element)) {
 					if (suspended) resume();
@@ -41,7 +57,7 @@ angular.module('hearth.directives').directive('suspendable', function() {
 				timeout = setTimeout(checkVisibility, 1000)
 			}
 
-			setTimeout(checkVisibility, 4000);
+			setTimeout(checkVisibility, 4000); // call main fction
 		}
 	}
 });
