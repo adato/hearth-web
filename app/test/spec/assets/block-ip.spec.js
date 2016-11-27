3e5fac7df1aa383dcf1b007ee5c7fa913a6de097
Jaroslav Vilc <jaroslav.vilc@topmonks.com>
2016-11-27 20:17:41 +0100

    HEARTH-943 User IP is blocked

---
+++ app/test/spec/assets/block-ip.spec.js
@@ -0,0 +1,36 @@
+describe('Access to Hearth blocked by IP address', function () {
+  var $window,
+    $httpBackend,
+    $http;
+
+  beforeEach(module('hearth', function ($provide) {
+    $provide.value('$window', {location: {href: ''}});
+  }));
+
+  // JSON returned from backend, when IP is blocked
+  var blockResponseMock = {"ok": false, "error": "IP blocked", "message": "Exceptions::IPBlocked"};
+
+    beforeEach(inject(function ($injector, _$window_, _$http_) {
+      $window = _$window_;
+      $http = _$http_;
+
+      $httpBackend = $injector.get('$httpBackend');
+
+      $httpBackend.whenGET('some_request').respond(403, blockResponseMock);
+
+      $httpBackend.whenGET(/^(http|https)/).respond();
+      $httpBackend.whenGET('locales/en/messages.json').respond(readJSON('app/locales/en/messages.json'));
+      $httpBackend.whenGET('assets/components/geo/markerTooltip.html').respond();
+      $httpBackend.whenGET('assets/pages/static/error404.html').respond();
+    }));
+
+
+  it('User is redirect to the access denied page', function () {
+    $http.get('some_request')
+      .then(function (response) {
+      });
+    $httpBackend.flush();
+
+    expect($window.location.href).toEqual('/access-denied.html');
+  });
+});
