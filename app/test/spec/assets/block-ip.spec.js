e7b06698373f67f9eb0073e0a39623e0dd6f2f0b
Jaroslav Vilc <jaroslav.vilc@topmonks.com>
2016-11-28 13:57:22 +0100

    HEARTH-943 add test for registration with blocked email

--- app/test/spec/assets/block-ip.spec.js
+++ app/test/spec/assets/block-ip.spec.js
@@ -10,20 +10,26 @@ describe('Access to Hearth blocked by IP address', function () {
   // JSON returned from backend, when IP is blocked
   var blockResponseMock = {"ok": false, "error": "IP blocked", "message": "Exceptions::IPBlocked"};
 
-    beforeEach(inject(function ($injector, _$window_, _$http_) {
-      $window = _$window_;
-      $http = _$http_;
+  beforeEach(inject(function ($injector, _$window_, _$http_) {
+    $window = _$window_;
+    $http = _$http_;
 
-      $httpBackend = $injector.get('$httpBackend');
+    $httpBackend = $injector.get('$httpBackend');
 
-      $httpBackend.whenGET('some_request').respond(403, blockResponseMock);
 
-      $httpBackend.whenGET(/^(http|https)/).respond();
-      $httpBackend.whenGET('locales/en/messages.json').respond('app/locales/en/messages.json');
-      $httpBackend.whenGET('assets/components/geo/markerTooltip.html').respond();
-      $httpBackend.whenGET('assets/pages/static/error404.html').respond();
-    }));
+    $httpBackend.whenGET('some_request').respond(403, blockResponseMock);
 
+    $httpBackend.whenGET(/^(http|https)/).respond();
+    $httpBackend.whenGET('locales/en/messages.json').respond('app/locales/en/messages.json');
+    $httpBackend.whenGET('assets/components/geo/markerTooltip.html').respond();
+    $httpBackend.whenGET('assets/pages/static/error404.html').respond();
+    $httpBackend.flush();
+  }));
+
+  afterEach(function () {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
 
   it('User is redirect to the access denied page', function () {
     $http.get('some_request')
@@ -32,5 +38,6 @@ describe('Access to Hearth blocked by IP address', function () {
     $httpBackend.flush();
 
     expect($window.location.href).toEqual('/access-denied.html');
+
   });
 });
