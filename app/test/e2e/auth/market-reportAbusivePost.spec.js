4076e5ca15dac4c668c91ade35d9fe88f7525146
Jaroslav Vilc <jaroslav.vilc@topmonks.com>
2016-12-14 13:04:24 +0100

    HEARTH-1074 marketplace is not loaded yet

--- app/test/e2e/auth/market-reportAbusivePost.spec.js
+++ app/test/e2e/auth/market-reportAbusivePost.spec.js
@@ -1,25 +1,26 @@
-describe('hearth report abusive post', function() {
-  beforeAll(function() {
+describe('hearth report abusive post', function () {
+  beforeAll(function () {
     protractor.helpers.login();
   });
 
-	it('should see "report abusive post" modal on market', function() {
-		var elAll = element.all(by.className('item-common')).get(0);
-		var expectedDropdown = elAll.element(by.css('ul.actions-dropdown'));
-		var dropdownReportPostLink = elAll.element(by.css('[test-beacon="marketplace-item-flag"]'));
-		var dropdownArrow = elAll.element(by.css('[test-beacon="marketplace-item-dropdown-toggle"]'));
-		var modal = element(by.css('div[test-beacon="flag-post-modal"]'));
-		var modalCloseButton = element(by.css('div[test-beacon="flag-post-modal"] a.close'));
+  it('should see "report abusive post" modal on market', function () {
+    browser.sleep(500);
+    var elAll = element.all(by.className('item-common')).get(0);
+    var expectedDropdown = elAll.element(by.css('ul.actions-dropdown'));
+    var dropdownReportPostLink = elAll.element(by.css('[test-beacon="marketplace-item-flag"]'));
+    var dropdownArrow = elAll.element(by.css('[test-beacon="marketplace-item-dropdown-toggle"]'));
+    var modal = element(by.css('div[test-beacon="flag-post-modal"]'));
+    var modalCloseButton = element(by.css('div[test-beacon="flag-post-modal"] a.close'));
 
-		dropdownArrow.click();
-		expect(expectedDropdown.isDisplayed()).toBeTruthy();
-		browser.sleep(100);
-		dropdownReportPostLink.click();
-		browser.sleep(1000);
-		expect(modal.isPresent()).toBeTruthy();
-		modalCloseButton.click();
-		browser.sleep(1000);
-		expect(modal.isPresent()).toBeFalsy();
-	});
+    dropdownArrow.click();
+    expect(expectedDropdown.isDisplayed()).toBeTruthy();
+    browser.sleep(100);
+    dropdownReportPostLink.click();
+    browser.sleep(1000);
+    expect(modal.isPresent()).toBeTruthy();
+    modalCloseButton.click();
+    browser.sleep(1000);
+    expect(modal.isPresent()).toBeFalsy();
+  });
 
 });
