be2febead44b3d24c7a4744894bb0c579a3ff30d
Pavel Voska <pavel.voska@gmail.com>
2017-01-16 11:25:26 +0100

    HEARTH-1113 better handling of navigation links

--- app/app/assets/pages/BaseCtrl.js
+++ app/app/assets/pages/BaseCtrl.js
@@ -316,8 +316,9 @@ angular.module('hearth.controllers').controller('BaseCtrl', [
 				query: null,
 				type: null
 			}, {
-				reload: true
+				reload: false
 			});
+			$scope.$broadcast('reloadMarketplace');
 		};
 
 		/**
