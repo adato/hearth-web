0dfd17672e9e2652b762c58217c66876d130c7a8
Pavel Voska <pavel.voska@gmail.com>
2017-01-17 10:09:52 +0100

    HEARTH-1106 also fixed OG title

--- app/app/assets/pages/item/ItemDetail.js
+++ app/app/assets/pages/item/ItemDetail.js
@@ -61,7 +61,7 @@ angular.module('hearth.controllers').controller('ItemDetail', [
 				// if there are post data, process them
 				if (data.text) {
 					var image = $rootScope.getSharingImage(data.attachments_attributes, data.author.avatar);
-					var postType = $filter('translate')($scope.postTypes[data.author._type][data.type]);
+					var postType = $filter('translate')($scope.postTypes[data.author._type][data.exact_type][data.type]);
 					var title = 'Hearth.net: ' + postType + ' ' + data.title + ' (' + data.author.name + ')';
 
 					OpenGraph.set(title, data.text || "", null, image.large, image.size);
