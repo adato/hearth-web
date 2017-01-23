99d2229b550c022bf87e72762e8ed54dfd49a8e0
Gaspari <gaspari@russiamail.com>
2017-01-23 13:02:47 +0100

    HEARTH-1128 limit > 5MB

--- app/app/assets/services/ProfileUtils.js
+++ app/app/assets/services/ProfileUtils.js
@@ -41,7 +41,7 @@ angular.module('hearth.services').factory('ProfileUtils', ['Karma', 'MottoLength
 	function getUploadOpts() {
 		return {
 			minSize: 400, // Pixels
-			limitMb: 2.5,
+			limitMb: 5,
 			resultPropName: 'public_avatar_url'
 		};
 	}
