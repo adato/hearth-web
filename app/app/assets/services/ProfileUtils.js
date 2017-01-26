78a12a582456d019e61160c90a5d27e396e2d18e
Gaspari <gaspari@russiamail.com>
2017-01-26 12:29:05 +0100

    file limit 5mb again

--- app/app/assets/services/ProfileUtils.js
+++ app/app/assets/services/ProfileUtils.js
@@ -42,7 +42,7 @@ angular.module('hearth.services').factory('ProfileUtils', ['Karma', 'MottoLength
 	function getUploadOpts() {
 		return {
 			minSize: 400, // Pixels
-			limitMb: 20,
+			limitMb: 5,
 			resultPropName: 'public_avatar_url'
 		};
 	}
