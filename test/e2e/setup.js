02208dcd947737399b4d3bf6eae00f01b3a83d3a
Gaspari <gaspari@russiamail.com>
2017-02-04 16:34:10 +0100

    route fix

--- test/e2e/setup.js
+++ test/e2e/setup.js
@@ -1,6 +1,6 @@
 var MailListener = require("mail-listener2");
 var Promise = require("es6-promise").Promise;
-const beacon = require('utils.js').beacon;
+const beacon = require('./utils.js').beacon;
 
 function getRandomInt (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
