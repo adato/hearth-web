ec99e5274a90638f482b51af7dd3245983be057d
Jaroslav Vilc <jaroslav.vilc@topmonks.com>
2017-03-07 14:40:44 +0100

    HEARTH-1199 fix protractor tests (#116)
    * HEARTH-1199 fix protractor tests
    
    * HEARTH-1199 fix karma tests, setup angular-mocks to same version as angular
    
    * HEARTH-1199 block ip test update
    
    * HEARTH-1199 fix mock urls

--- test/unit/assets/block-ip.spec.js
+++ test/unit/assets/block-ip.spec.js
@@ -1,29 +1,23 @@
 describe('Access to Hearth blocked by IP address', function () {
-  var $window;
   var $httpBackend;
   var $http;
-
-  beforeEach(module('hearth', function ($provide) {
-    $provide.value('$window', {location: {href: ''}});
-  }));
+  var $location;
 
   // JSON returned from backend, when IP is blocked
   var blockResponseMock = {"ok": false, "error": "IP blocked", "message": "Exceptions::IPBlocked"};
 
-  beforeEach(inject(function ($injector, _$window_, _$http_) {
-    $window = _$window_;
+  beforeEach(inject(function ($injector, _$http_, _$location_, _$httpBackend_) {
     $http = _$http_;
+    $location = _$location_;
 
-    $httpBackend = $injector.get('$httpBackend');
+    $httpBackend = _$httpBackend_;
 
-
-    $httpBackend.whenGET('some_request').respond(403, blockResponseMock);
+    $httpBackend.whenGET(PATH.EXPECT_ACCESS_DENIED).respond(403, blockResponseMock);
 
     $httpBackend.whenGET(/^(http|https)/).respond();
     $httpBackend.whenGET(PATH.MESSAGES_JSON_EN_WHEN).respond(PATH.MESSAGES_JSON_EN_RESPONSE);
     $httpBackend.whenGET('assets/components/geo/markerTooltip.html').respond();
     $httpBackend.whenGET('assets/pages/static/error404.html').respond();
-    $httpBackend.flush();
   }));
 
   afterEach(function () {
@@ -32,12 +26,11 @@ describe('Access to Hearth blocked by IP address', function () {
   });
 
   it('User is redirect to the access denied page', function () {
-    $http.get('some_request')
-      .then(function (response) {
+    $http.get(PATH.EXPECT_ACCESS_DENIED)
+      .then(() => {
+        expect($location.path()).toEqual('/app/access-denied.html');
       });
+
     $httpBackend.flush();
-
-    expect($window.location.href).toEqual('/app/access-denied.html');
-
   });
 });
--- test/unit/setup.js
+++ test/unit/setup.js
@@ -2,5 +2,6 @@
 const PATH = {
   API_SESSION: /^https:\/\/api.(?:dev\.|stage\.|)hearth.net\/session\?.*/,
   MESSAGES_JSON_EN_WHEN: 'assets/locale/en.json',
-  MESSAGES_JSON_EN_RESPONSE: 'app/assets/locale/en.json'
+  MESSAGES_JSON_EN_RESPONSE: 'app/assets/locale/en.json',
+  EXPECT_ACCESS_DENIED: 'some_request'
 };
