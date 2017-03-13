f52a23a8c61365c45a1a9d3a51cb5df8d72a70ca
Gaspari <gaspari@russiamail.com>
2017-03-13 08:40:46 +0100

    HEARTH-1144 lp marketplace changed

--- landing/js/characters.js
+++ landing/js/characters.js
@@ -1,22 +1,26 @@
 ;(function(window, config) {
 	'use strict';
 
-	var requestApi = window.aeg.requestApi;
-	var $ = window.aeg.$;
-	var fe = window.aeg.fe;
-  var apiPath = config.apiPath;
+	const requestApi = window.aeg.requestApi;
+	const $ = window.aeg.$;
+	const fe = window.aeg.fe;
+  const apiPath = config.apiPath;
 
-  var CHAR_FILL = 'character-fill';
-  var CHARACTER_FILL_SELECTOR = '[' + CHAR_FILL + ']';
+  const CHAR_FILL = 'character-fill';
+  const CHARACTER_FILL_SELECTOR = '[' + CHAR_FILL + ']';
+
+  const NUMBER_OF_GIFTS_TRANSLATION_SELECTOR = 'translation[rel="NUMBER_OF_GIFTS"]';
+  const NUMBER_VARIABLE = '{{count}}';
+  const NUMBER_OF_GIFTS_TRANSLATION = $(NUMBER_OF_GIFTS_TRANSLATION_SELECTOR)[0].innerHTML;
 
   fe($(CHARACTER_FILL_SELECTOR), item => {
-    var character = item.getAttribute(CHAR_FILL);
-    var req = requestApi('GET', apiPath + '/search?counters=true&type=post&character=' + character);
+    const character = item.getAttribute(CHAR_FILL);
+    const req = requestApi('GET', apiPath + '/search?counters=true&type=post&character=' + character);
 		req.onload = function() {
       if (req.status === 200) {
-        var res = JSON.parse(req.responseText);
+        const res = JSON.parse(req.responseText);
         if (!(res && res.counters && res.counters.post)) return console.info('derp');
-        item.innerHTML = '(' + res.counters.post + ')';
+        item.innerHTML = NUMBER_OF_GIFTS_TRANSLATION.replace(NUMBER_VARIABLE, res.counters.post);
       } else {
         console.error('Failed to fetch item counts for character "' + character + '"');
       }
