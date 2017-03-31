7d46ea9031bf62e0e4af43e21a60e577aab6d6d3
Gaspari <gaspari@russiamail.com>
2017-03-31 13:44:54 +0200

    HEARTH-1229 fb authorizaton ep

--- app/assets/app.js
+++ app/assets/app.js
@@ -107,6 +107,7 @@ angular.module('hearth', [
 			$authProvider.facebook({
 				clientId: $$config.oauth.facebook,
 				url: $window.encodeURI($$config.apiPath + '/auth/facebook' + $window.refsString),
+  			authorizationEndpoint: 'https://www.facebook.com/v2.8/dialog/oauth'
 			});
 
 			$authProvider.google({
@@ -229,8 +230,19 @@ angular.module('hearth', [
 								path: '/'
 							});
 
-							Auth.logout(function() {
-								location.reload("/login");
+							// Auth.logout(function() {
+							// 	location.reload('/login');
+							// });
+							function cb() {
+								location.reload('/login');
+							}
+							Session.get(session => {
+								if (session._id) delete session._id;
+								// $http.post($$config.apiPath + '/logout').success(cb).error(cb);
+								User.logout({}, cb, cb);
+							}, err => {
+								// $http.post($$config.apiPath + '/logout').success(cb).error(cb);
+								User.logout({}, cb, cb);
 							});
 						}
 
