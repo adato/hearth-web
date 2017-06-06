67df047cd23a849d7c9ca4176a592a6c2f2e5c2f
Anton Gaspari <Gaspari@users.noreply.github.com>
2017-06-06 09:42:51 +0200

    HEARTH-1317 terms (#215)
    * HEARTH-1317 path fixed
    
    * HEARTH-1317 fixed path, removed some useless stuff

--- app/assets/pages/BaseCtrl.js
+++ app/assets/pages/BaseCtrl.js
@@ -534,8 +534,8 @@ angular.module('hearth.controllers').controller('BaseCtrl', [
 		};
 
 		$rootScope.showTerms = function() {
-			$rootScope.openModalContainer('/app/locales/' + $rootScope.language + '/terms.html', 'MENU.TERMS');
-		};
+			$rootScope.openModalContainer('assets/locales/' + $rootScope.language + '/terms.html', 'MENU.TERMS')
+		}
 
 		/**
 		 * Function will show modal window where community admin can remove post from his community
