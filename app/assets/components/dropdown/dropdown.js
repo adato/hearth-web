e2940dc617a794a81d69aa52f0bae49faf6b18b2
Anton Gaspari <Gaspari@users.noreply.github.com>
2017-06-07 18:50:49 +0200

    HEARTH-1344 new dropdown-scope attr for dropdown (#226)

--- app/assets/components/dropdown/dropdown.js
+++ app/assets/components/dropdown/dropdown.js
@@ -13,47 +13,58 @@ angular.module('hearth.directives').directive('dropdown', ['$document', '$timeou
 		restrict: 'A',
 		scope: {
 			dropdown: '@', // class or id of element to show/hide
-			hover: '=' // show/hide dropdown also on hover event
+			hover: '=', // show/hide dropdown also on hover event
+			dropdownScope: '='
 		},
 		link: function($scope, el, attrs) {
 
+			var dropdownSearchScope = findScope()
+
 			// on element click toggle dropdown
 			el.on('click', event => {
-				const action = getTarget().css('display') == 'block' ? hideDropdown : showDropdown;
-				hideDropdown();
-				$timeout(action);
-			});
+				const action = getTarget().css('display') == 'block' ? hideDropdown : showDropdown
+				hideDropdown()
+				$timeout(action)
+			})
 
 			if ($scope.hover) {
-				el.parent().on('mouseenter', showDropdown);
-				el.parent().on('mouseleave', hideDropdown);
+				el.parent().on('mouseenter', showDropdown)
+				el.parent().on('mouseleave', hideDropdown)
 			}
 
 			// when clicking outside of dropdown label, hide dropdown
-			$document.on('click', e => DOMTraversalService.hasParent(e.target, el) ? false : hideDropdown());
+			$document.on('click', e => DOMTraversalService.hasParent(e.target, el) ? false : hideDropdown())
 
 			// clean-up
 			$scope.$on('$destroy', () => {
-				$document.off('click', hideDropdown);
-			});
+				$document.off('click', hideDropdown)
+			})
 
 			///////////////
 
 			// return the dropdown element to show/hide
 			function getTarget() {
-				return $document.find($scope.dropdown);
+				return dropdownSearchScope.find($scope.dropdown)
 			}
 
 			function showDropdown() {
-				getTarget().css('display', 'block');
+				getTarget().css('display', 'block')
 			}
 
 			// hide dropdown
 			function hideDropdown() {
-				getTarget().css('display', 'none');
+				getTarget().css('display', 'none')
+			}
+
+			function findScope() {
+				var scope
+				if ($scope.dropdownScope) {
+					scope = DOMTraversalService.findParentBySelector(el[0], $scope.dropdownScope)
+				}
+				return scope ? angular.element(scope) : $document
 			}
 
 		}
 	}
 
-}]);
+}])
