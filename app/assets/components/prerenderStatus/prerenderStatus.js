05d3bbf02769504419a07cedd4373ec71e967f36
Gaspari <gaspari@russiamail.com>
2017-06-12 18:38:57 +0200

    HEARTH-1200 controller for link

--- app/assets/components/prerenderStatus/prerenderStatus.js
+++ app/assets/components/prerenderStatus/prerenderStatus.js
@@ -8,18 +8,23 @@
  */
 
 angular.module('hearth.directives').directive('prerenderStatus', ['PrerenderService', function(PrerenderService) {
+
   return {
     restrict: 'AE',
     scope: {
     },
-    template: '<meta name="prerender-status-code" content="{{vm.status.statusCode}}" ng-if="vm.allowedStatuses[vm.status.statusCode]">',
+    template: '<meta name="prerender-status-code" content="{{vm.status.statusCode}}" ng-if="vm.allowedStatuses[vm.status.statusCode]" />',
     controllerAs: 'vm',
-    link: function(scope, element, attrs) {
+    controller: [function() {
+
       const vm = this
+
+      vm.status = PrerenderService
       vm.allowedStatuses = {
         '404' : true
       }
-      vm.status = PrerenderService
-    }
-  };
-}]);
+
+    }]
+  }
+
+}])
