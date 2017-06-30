da31b6b48d2b87f8e810aebe832ed89cc801c504
Gaspari <gaspari@russiamail.com>
2017-06-30 11:27:29 +0200

    HEARTH-1407 typo fix

--- landing/js/profile.js
+++ landing/js/profile.js
@@ -1,76 +1,76 @@
 ;(function(window, config) {
 	'use strict';
 
-	const $ = window.aeg.$;
-	const cookieFactory = window.aeg.cookieFactory;
- 	const fe = window.aeg.fe;
-	const requestApi = window.aeg.requestApi;
+	const $ = window.aeg.$
+	const cookieFactory = window.aeg.cookieFactory
+ 	const fe = window.aeg.fe
+	const requestApi = window.aeg.requestApi
 
 	//
 	//	PROFILE
 	//
-	var profile;
-	const apiPath = config.apiPath;
-	const authTokenIdentificator = config.authTokenIdentificator;
+	var profile
+	const apiPath = config.apiPath
+	const authTokenIdentificator = config.authTokenIdentificator
 
-	const loggedSelector = '[user-logged]';
-	const notLoggedSelector = '[user-not-logged]';
-	const profileSectionSelector = '#profileSection';
-	const profileAvatarSelector = '[profile-image]';
-	const profileAvatarDefault = '/img/no-avatar.jpg';
-	const profileFullNameSelector = '[profile-name]';
-	const profileLinkSelector = '[profile-link]';
-	const logoutNodeIdentificator = '[logout]';
-	const unreadMessagesBadgeSelector = '[message-count]';
+	const loggedSelector = '[user-logged]'
+	const notLoggedSelector = '[user-not-logged]'
+	const profileSectionSelector = '#profileSection'
+	const profileAvatarSelector = '[profile-image]'
+	const profileAvatarDefault = '/img/no-avatar.jpg'
+	const profileFullNameSelector = '[profile-name]'
+	const profileLinkSelector = '[profile-link]'
+	const logoutNodeIdentificator = '[logout]'
+	const unreadMessagesBadgeSelector = '[message-count]'
 
 	// COOKIE PATH ERROR FIX-UP
-	cookieFactory.remove(authTokenIdentificator, '/cs');
-	cookieFactory.remove(authTokenIdentificator, '/sk');
+	cookieFactory.remove(authTokenIdentificator, '/cs')
+	cookieFactory.remove(authTokenIdentificator, '/sk')
 
-	var apiToken = cookieFactory.get(authTokenIdentificator);
+	var apiToken = cookieFactory.get(authTokenIdentificator)
 	if (apiToken) {
-		initProfile(apiToken);
+		initProfile(apiToken)
 	} else {
-		fe($(notLoggedSelector), function(el) {el.style.display = 'inherit'; el.classList.add('inited');});
-		fe($(loggedSelector), function(el) {el.style.display = 'none'; el.classList.add('inited');});
-		initCharacterLinks();
+		fe($(notLoggedSelector), function(el) {el.style.display = 'inherit'; el.classList.add('inited')})
+		fe($(loggedSelector), function(el) {el.style.display = 'none'; el.classList.add('inited')})
+		initCharacterLinks()
 	}
 
 	// expose logout function
-	window.logout = logout;
+	window.logout = logout
 
-	initLogoutFunction();
+	initLogoutFunction()
 
 	///////////////////
 
 	function initProfile(apiToken) {
 		// get profile information
-		var req = requestApi('GET', apiPath + '/profile', {token: apiToken});
+		var req = requestApi('GET', apiPath + '/profile', {token: apiToken})
 		req.onload = function() {
 			if (req.status === 200) {
-				profile = JSON.parse(req.responseText);
-				// console.log(profile);
-				fillProfile(profile);
+				profile = JSON.parse(req.responseText)
+				// console.log(profile)
+				fillProfile(profile)
 			} else {
-				profileNotLogged();
-				console.log('Profile request failed. Returned status of ' + req.status);
+				profileNotLogged()
+				console.log('Profile request failed. Returned status of ' + req.status)
 			}
-			initCharacterLinks(profile);
-		};
-		req.send();
+			initCharacterLinks(profile)
+		}
+		req.send()
 
 		// get unread message count
-		var unreadMessageCountReq = requestApi('GET', apiPath + '/conversations/counter', {token: apiToken});
+		var unreadMessageCountReq = requestApi('GET', apiPath + '/conversations/counter', {token: apiToken})
 		unreadMessageCountReq.onload = function() {
 			if (unreadMessageCountReq.status === 200) {
-				var res = JSON.parse(unreadMessageCountReq.responseText);
+				var res = JSON.parse(unreadMessageCountReq.responseText)
 				if (res && res.unread !== void 0) {
-					var unreadCount = parseInt(res.unread) > 1000 ? '999+' : res.unread;
-					fe($(unreadMessagesBadgeSelector), function(el) {el.innerHTML = unreadCount; el.style.display = 'block'});
+					var unreadCount = parseInt(res.unread) > 1000 ? '999+' : res.unread
+					fe($(unreadMessagesBadgeSelector), function(el) {el.innerHTML = unreadCount; el.style.display = 'block'})
 				}
 			}
 		}
-		unreadMessageCountReq.send();
+		unreadMessageCountReq.send()
 	}
 
 	/**
@@ -96,7 +96,7 @@
 	function fillProfile(profileObject) {
 		profileLogged()
 
-		fe($(profileAvatarSelector), function(el) {el.setAttribute('src', (profile.avatar ? profile.avatar.normal : profileAvatarDefault)})
+		fe($(profileAvatarSelector), function(el) {el.setAttribute('src', (profile.avatar ? profile.avatar.normal : profileAvatarDefault))})
 		fe($(profileFullNameSelector), function(el) {el.innerHTML = [profile.name, profile.surname].join('\u00A0').trim()})
 		fe($(profileLinkSelector), function(el) {el.setAttribute('href', '/app/profile/' + profile._id)})
 	}
@@ -125,15 +125,15 @@
 	function initLogoutFunction() {
 		fe($(logoutNodeIdentificator), function(el) {
 			if (el.tagName.toLowerCase() === 'a') {
-				el.setAttribute('href', 'javascript:logout()');
+				el.setAttribute('href', 'javascript:logout()')
 			} else {
 				el.addEventListener('click', logout)
 			}
-		});
+		})
 	}
 
 	function initCharacterLinks(profile) {
-		if (typeof window.marketplaceMasonryInit === 'function') window.marketplaceMasonryInit({additionalParams: 'lang=' + (profile ? (profile.user_languages.join(',') + '&my-lang=1') : document.querySelector('html').getAttribute('lang'))});
+		if (typeof window.marketplaceMasonryInit === 'function') window.marketplaceMasonryInit({additionalParams: 'lang=' + (profile ? (profile.user_languages.join(',') + '&my-lang=1') : document.querySelector('html').getAttribute('lang'))})
 	}
 
 })(window, window.hearthConfig);
