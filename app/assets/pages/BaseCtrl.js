ea73ee3c9d8df14c2e7b5e3cac62d78651b6e4ea
Gaspari <gaspari@russiamail.com>
2017-07-04 15:08:41 +0200

    HEARTH-1404 comment fix

--- app/assets/pages/BaseCtrl.js
+++ app/assets/pages/BaseCtrl.js
@@ -109,7 +109,7 @@ angular.module('hearth.controllers').controller('BaseCtrl', [
 				locationSearch = void 0
 			}
 
-			// if a new version is available - make a reload instead of just a state change
+			// if a new version is available - make a reload to the target state instead of just a state change
 			if (ApiHealthChecker.shouldReload()) {
 				event.preventDefault()
 				return $window.location = $state.href(toState, toParams)
