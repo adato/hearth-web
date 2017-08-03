9e0469a5fccc112c4babe81c18c3980de5f794eb
Anton Gaspari <Gaspari@users.noreply.github.com>
2017-08-03 10:53:18 +0200

    HEARTH-1448 post comments (#301)
    * HEARTH-1448 post comments
    
    * HEARTH-1448 improvements
    
    * HEARTH-1448 comment avatars path fixed
    
    * HEARTH-1448 post count fix
    
    * HEARTH-1448 post comments improvements
    
    * HEARTH-1448 local sync

--- app/assets/components/ctrlEnterSubmit/ctrlEnterSubmit.js
+++ app/assets/components/ctrlEnterSubmit/ctrlEnterSubmit.js
@@ -5,6 +5,10 @@
  * @description Directive that submits a form (or performs other action) on ctrl/meta-key + enter
  *		It may take options in an attribute ctrl-enter-submit-opts, which is an object with possible props:
  *			- {Boolean} refocus - if true, refocuses the element after submitting
+ * @example
+ *		<form ng-submit='vm.asdf()' name='vm.supaform'>
+ *			<textarea ctrl-enter-submit ng-model='vm.kek'></textarea>
+ *		</form>
  * @restrict A
  */
 angular.module('hearth.directives').directive('ctrlEnterSubmit', ['$timeout',
@@ -14,48 +18,46 @@ angular.module('hearth.directives').directive('ctrlEnterSubmit', ['$timeout',
 			link: link,
 			require: '^?form',
 			restrict: 'A'
-		});
+		})
 
 		function link(scope, element, attributes, formController) {
-			if (!attributes.ctrlEnterSubmit && !formController) return false;
-			element.on('keydown', handleKeyEvent);
+			if (!attributes.ctrlEnterSubmit && !formController) return false
+			element.on('keydown', handleKeyEvent)
 
 			function handleKeyEvent(event) {
 				if ((event.which === 13) && (event.metaKey || event.ctrlKey)) {
-					event.preventDefault();
-					attributes.ctrlEnterSubmit ? triggerExpression(event) : triggerFormEvent();
+					event.preventDefault()
+					attributes.ctrlEnterSubmit ? triggerExpression(event) : triggerFormEvent()
 				}
 			}
 
 			function triggerExpression(keyEvent) {
 				scope.$apply(
 					function changeViewModel() {
-						scope.$eval(attributes.ctrlEnterSubmit, {
-							$event: keyEvent
-						});
+						scope.$eval(attributes.ctrlEnterSubmit, {$event: keyEvent})
 					}
-				);
+				)
 			}
 
 			function triggerFormEvent() {
-				closestForm().triggerHandler('submit');
+				closestForm().triggerHandler('submit')
 				if (attributes.ctrlEnterSubmitOpts) {
-					var opts = scope.$eval(attributes.ctrlEnterSubmitOpts);
+					var opts = scope.$eval(attributes.ctrlEnterSubmitOpts)
 					if (opts.refocus) $timeout(function() {
-						element[0].focus();
-					});
+						element[0].focus()
+					})
 				}
 			}
 
 			function closestForm() {
-				var parent = element.parent();
+				var parent = element.parent()
 				while (parent.length && (parent[0].tagName !== 'FORM')) {
-					parent = parent.parent();
+					parent = parent.parent()
 				}
-				return (parent);
+				return (parent)
 			}
 
 		}
 
 	}
-]);
+])
---
+++ app/assets/components/post/postComments/directives/postComments.js
@@ -0,0 +1,95 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name hearth.directives.postComments
+ * @description Directive that provides UI for comments to posts
+ * @restrict E
+ */
+angular.module('hearth.directives').directive('postComments', [function() {
+
+	return {
+		restrict: 'E',
+    templateUrl: 'assets/components/post/postComments/templates/postComments.html',
+    scope: {},
+    bindToController: {
+      model: '=',
+      postId: '='
+    },
+    controllerAs: 'vm',
+    controller: ['Post', '$rootScope', '$timeout', '$filter', function(Post, $rootScope, $timeout, $filter) {
+
+      const ctrl = this
+
+      const STATUS_TIMEOUT = 2000
+			const POST_COMMENT_NEW = 'post-comment-new'
+
+      ctrl.$onInit = () => {
+
+        ctrl.loggedUser = $rootScope.loggedUser
+
+        ctrl.message
+        ctrl.sending
+        ctrl.success
+        ctrl.error
+
+        ctrl.submit = submitMessage
+
+        init()
+
+      }
+
+      /////////////////
+
+      function init() {
+        ctrl.model = ctrl.model || []
+        Post.queryComments({postId: ctrl.postId}).$promise
+        .then(res => {
+          res && res.length && (ctrl.model = res.map(comment => {
+						comment.created_at_timeago = $filter('ago')(comment.created_at)
+						return comment
+					}))
+        })
+        .catch(err => {
+          console.log('failed to query comments', err)
+        })
+      }
+
+      function submitMessage(message) {
+        if (!message || ctrl.sending) return
+
+        ctrl.sending = true
+        Post.createComment({postId: ctrl.postId}, {comment: {text: message}}).$promise
+        .then(res => {
+          ctrl.message = ''
+
+					res.created_at_timeago = $filter('ago')(res.created_at)
+          ctrl.model.push(res)
+
+          ctrl.success = true
+          $timeout(() => ctrl.success = false, STATUS_TIMEOUT)
+
+					$rootScope.$emit(POST_COMMENT_NEW, {postId: ctrl.postId, newComment: res, commentList: angular.copy(ctrl.model)})
+        })
+        .catch(err => {
+          console.log('failed to create a post comment', err)
+
+          ctrl.error = true
+          $timeout(() => ctrl.error = false, STATUS_TIMEOUT)
+        })
+        .finally(() => {
+          ctrl.sending = false
+        })
+      }
+
+			function commentContained({arr, comment}) {
+				for (var i = arr.length;i--;) {
+					if (arr[i]._id === comment._id) return true
+				}
+				return false
+			}
+
+    }]
+  }
+
+}])
