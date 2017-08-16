9173a034afbf161da010d03e4d0188d8968cd44b
Anton Gaspari <Gaspari@users.noreply.github.com>
2017-08-16 15:55:43 +0200

    HEARTH-1457 avatar tplt dyplicated without one-time binding (#313)

--- app/assets/components/avatar/avatar.js
+++ app/assets/components/avatar/avatar.js
@@ -4,9 +4,14 @@
  * @ngdoc directive
  * @name hearth.directives.avatar
  * @description component that creates an avatar (user or community)
+ * @opts
+ *		src - url to avatar
+ *		size - css class - see avatar.scss
+ *		type - community or user
+ *		href - if present, the avatar will be a link with its value as its href, div otherwise
+ *		dynamic - if present, the template will not be one time bound
  * @restrict E
  */
-
 angular.module('hearth.directives').directive('avatar', [function() {
 
 	return {
@@ -18,7 +23,10 @@ angular.module('hearth.directives').directive('avatar', [function() {
 			'type': '=',
 			'href': '='
 		},
-		templateUrl: 'assets/components/avatar/avatar.html',
+		templateUrl: (el, attrs) => {
+			const template = attrs.dynamic !== void 0 ? 'avatar-dynamic' : 'avatar'
+			return `assets/components/avatar/${template}.html`
+		},
 		link: function($scope) {
 			$scope.class = "avatar-" + ($scope.size || 'normal') + ' ' + (($scope.type === 'Community') ? 'avatar-community' : 'avatar-user')
 		}
