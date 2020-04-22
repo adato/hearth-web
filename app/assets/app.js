486d7ee70b9decdd7b96812050315e156558ce72
Pavel Voska <pavel.voska@adato.org>
2020-04-22 12:52:39 +0000

    continue pruning

--- app/assets/app.js
+++ app/assets/app.js
@@ -208,21 +208,6 @@ angular.module('hearth', [
 					// if is logged, check if he wanted to see some restricted page before login
 					if ($rootScope.loggedUser._id) { UnauthReload.goToSavedLocationIfAny(); }
 
-					if (typeof mixpanel !== 'undefined') { // verify if mixpanel is present, prevent fail with adblock
-						if ($rootScope.loggedUser && $rootScope.loggedUser._id) {
-							mixpanel.identify($rootScope.loggedUser._id);
-							mixpanel.people.set({
-								"$name": $rootScope.loggedUser.name,
-								"$email": $rootScope.loggedUser.email,
-								"$device-type": getDevice()
-							});
-						} else {
-							mixpanel.people.set({
-								"$device-type": getDevice()
-							});
-						}
-					}
-
 					$rootScope.$broadcast("initSessionSuccess", $rootScope.loggedUser);
 					done(null, $rootScope.loggedUser);
 				}, function(err) {
